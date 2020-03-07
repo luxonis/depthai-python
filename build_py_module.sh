47e560630dae29f43c4e490fb579b816c407efcf
alex-luxonis <alex@luxonis.com>
2020-03-06 19:19:25 -0700

    Fix build hang for memory constrained hosts

--- build_py_module.sh
+++ build_py_module.sh
@@ -3,7 +3,15 @@
 INITIAL_DIR=`pwd`
 SOURCE_DIR=$(dirname "$0")
 BUILD_DIR=build
-NUM_CORES=`nproc`
+
+# Default 'make' to a number of jobs equal to CPU threads, but lower it
+# if the memory available is too low, to avoid a lock up (e.g for RPi)
+NUM_JOBS=`nproc`
+MEM_AVAIL=`grep MemAvailable /proc/meminfo | awk '{print int($2/1024)}'`
+echo "MemAvailable: $MEM_AVAIL MB"
+if   [[ $MEM_AVAIL -lt  900 ]]; then NUM_JOBS=1
+elif [[ $MEM_AVAIL -lt 1400 ]]; then NUM_JOBS=2
+fi
 
 print_exec() {
     REL_PATH="realpath --relative-to=$INITIAL_DIR `pwd`"
@@ -24,7 +32,7 @@ if [ ! -f Makefile ]; then
     print_exec cmake ..
 fi
 
-print_exec make -j$NUM_CORES
+print_exec make -j$NUM_JOBS
 
 DEPTHAI_LIB=(*.so)
 print_exec cp ${DEPTHAI_LIB[0]} ../../../../
