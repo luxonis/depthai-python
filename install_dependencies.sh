b17e90784ed94b8f0202ac04dd3481797ed6e8cc
alex-luxonis <alex@luxonis.com>
2020-03-06 19:12:22 -0700

    install_dependencies.sh: fix check when ran from other dir (e.g: ./depthai-api/install_dependencies.sh)

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,14 +1,16 @@
 SOURCE_DIR=$(dirname "$0")
 
+cd $SOURCE_DIR
+
 if [ ! -d "shared/3rdparty/boost_1_71_0" ]
 then
     echo "boost: installing ..."
-    cd "$SOURCE_DIR/shared/3rdparty/"
+    cd "shared/3rdparty/"
     wget https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.bz2
     echo "boost: unpacking."
     tar xjf boost_1_71_0.tar.bz2
     rm boost_1_71_0.tar.bz2
-    cd ../../..
+    cd ../..
     echo "boost: installed."
 else
     echo "boost: already installed."
