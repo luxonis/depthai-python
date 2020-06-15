1faef5bfbdde3d8471e78a13fb73f9e8d9384427
Martin Peterlin <martin.peterlin7@gmail.com>
2020-06-15 18:11:04 +0200

    Bump version

--- shared/version.hpp
+++ shared/version.hpp
@@ -1,4 +1,4 @@
 #pragma once
  
-const char *c_depthai_dev_version = "b185d1de77a36055fa7fff1ed638685eba7c8bb0";
-const char *c_depthai_version = "0.0.10a";
+const char *c_depthai_dev_version = "17d0aaf9269019040108840f7846b37ac8164499";
+const char *c_depthai_version = "0.1.0";
