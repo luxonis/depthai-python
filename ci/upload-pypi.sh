1740d97bd0d5a3b45573c70f5e04b69b9ce1c928
Łukasz Piłatowski <lukpila29@gmail.com>
2020-06-16 23:06:03 +0200

    quote pypi server

--- ci/upload-pypi.sh
+++ ci/upload-pypi.sh
@@ -3,5 +3,5 @@ python3 -m pip install -U twine
 
 for file in depthai-*whl; do
   echo "Uploading $file"
-  python3 -m twine upload --repository-url $PYPI_SERVER --username $PYPI_USER --password $PYPI_PASSWORD $file
+  python3 -m twine upload --repository-url "$PYPI_SERVER" --username $PYPI_USER --password $PYPI_PASSWORD $file
 done
