a17463c59108baacfa5c740ea8e208f259f09d76
Łukasz Piłatowski <lukpila29@gmail.com>
2020-06-16 12:24:52 +0200

    try artifactory upload

---
+++ ci/upload-artifactory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export BUILD_NAME=gitlabci-depthai-device-side-snapshot-build
+export PATH_PREFIX=luxonis-myriad-snapshot-local/depthai-device-side
+
+curl -fL https://getcli.jfrog.io | sh
+./jfrog rt config --url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
+./jfrog rt u "depthai-0.0.1-cp*" "$PATH_PREFIX/$GITHUB_SHA/"
+./jfrog rt u "wheelhouse/*" "$PATH_PREFIX/$GITHUB_SHA/"
