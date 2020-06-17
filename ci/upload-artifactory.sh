b979e718034831e33e8f39323788d6eeef974e4c
Łukasz Piłatowski <lukpila29@gmail.com>
2020-06-17 12:30:50 +0200

    fix upload path returning 404

--- ci/upload-artifactory.sh
+++ ci/upload-artifactory.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd wheelhouse/audited/ || exit 1
 export PATH_PREFIX=luxonis-python-snapshot-local/depthai
 
 curl -fL https://getcli.jfrog.io | sh
 ./jfrog rt config --url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
-./jfrog rt u "wheelhouse/audited/*" "$PATH_PREFIX/"
+./jfrog rt u "*" "$PATH_PREFIX/"
