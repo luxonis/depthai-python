7fa77bc39835f6ddacb4e940a8cce72e97a4cba7
Łukasz Piłatowski <lukpila29@gmail.com>
2020-06-17 11:59:08 +0200

    change PyPi and artifactory upload

--- ci/upload-artifactory.sh
+++ ci/upload-artifactory.sh
@@ -4,4 +4,4 @@ export PATH_PREFIX=luxonis-python-snapshot-local/depthai
 
 curl -fL https://getcli.jfrog.io | sh
 ./jfrog rt config --url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
-./jfrog rt u "depthai*" "$PATH_PREFIX/"
+./jfrog rt u "wheelhouse/audited/*" "$PATH_PREFIX/"
--- ci/upload-pypi.sh
+++ ci/upload-pypi.sh
@@ -2,7 +2,7 @@
 python3 -m pip install -U twine
 
 echo "[TEST] PYPI_SERVER: $PYPI_SERVER ; PYPI_USER: $PYPI_USER ; PYPI_PASSWORD: $PYPI_PASSWORD"
-for file in depthai-*whl; do
+for file in wheelhouse/audited/*.whl; do
   echo "Uploading $file"
   python3 -m twine upload --repository-url "$PYPI_SERVER" --username $PYPI_USER --password $PYPI_PASSWORD $file
 done
