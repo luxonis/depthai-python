01ff899b43913359af7f353f43d5c3319fa33b32
Łukasz Piłatowski <lukpila29@gmail.com>
2020-06-17 11:25:57 +0200

    fix artifactory upload

--- ci/upload-artifactory.sh
+++ ci/upload-artifactory.sh
@@ -4,4 +4,4 @@ export PATH_PREFIX=luxonis-python-snapshot-local/depthai
 
 curl -fL https://getcli.jfrog.io | sh
 ./jfrog rt config --url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
-./jfrog rt u "wheelhouse/audited/(*)" "$PATH_PREFIX/{1}"
+./jfrog rt u "depthai*" "$PATH_PREFIX/"
