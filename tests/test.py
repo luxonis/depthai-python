611bd0ded802e136acc566cd3ea30cf3c690b54b
Łukasz Piłatowski <lukpila29@gmail.com>
2020-06-18 11:59:46 +0200

    remove pipeline test

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,4 @@
 import unittest
-from pathlib import Path
 
 
 class TestDepthAI(unittest.TestCase):
@@ -17,23 +16,6 @@ class TestDepthAI(unittest.TestCase):
             del device
         except:
             print("Unable to init DepthAI device!")
-            raise
-
-    def test_create_pipeline(self):
-        import depthai
-        device = depthai.Device("", False)
-        try:
-            pipeline = device.create_pipeline({
-                'streams': ['previewout'],
-                'ai': {
-                    'blob_file': str(Path('./mobilenet-ssd.blob').resolve().absolute()),
-                    'blob_file_config': str(Path('./mobilenet-ssd.json').resolve().absolute())
-                }
-            })
-            assert pipeline is not None, "Unable to create DepthAI pipeline"
-            del pipeline
-        except:
-            print("Unable to create pipeline!")
             raise
 
 
