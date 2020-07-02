9f3e87aa3d93270625c37239666a1162b66698b3
alex-luxonis <alex@luxonis.com>
2020-07-02 15:46:55 +0300

    Append a _stage2 suffix to 2nd stage tensor name, to prevent potential name collision (with 1st stage tensor)

--- host/core/nnet/tensor_info_helper.hpp
+++ host/core/nnet/tensor_info_helper.hpp
@@ -89,12 +89,22 @@ bool parseTensorInfosFromJsonString(const std::string& json_str, std::vector<Ten
             break;
         }
 
+        // Try to determine the current stage based on the size of tensor_infos
+        int stage = tensor_infos.size() + 1;
+
         for (auto &tensor_it : json_obj.at("tensors"))
         {
             // TODO: check content format !!!
 
             TensorInfo tensor_info;
             tensor_info.output_tensor_name = tensor_it.at("output_tensor_name").get<std::string>();
+            if (stage > 1) {
+                printf("INFO: Suffixing output_tensor_name for stage %d: %s",
+                        stage, tensor_info.output_tensor_name.c_str());
+                tensor_info.output_tensor_name += "_stage";
+                tensor_info.output_tensor_name += '0' + stage;
+                printf(" -> %s\n", tensor_info.output_tensor_name.c_str());
+            }
             for (auto &v : tensor_it.at("output_dimensions")) { tensor_info.output_dimensions.push_back(v.get<unsigned>()); }
             tensor_info.output_entry_iteration_index = tensor_it.at("output_entry_iteration_index").get<unsigned>();
             for (auto &v : tensor_it.at("output_properties_dimensions")) { tensor_info.output_properties_dimensions.push_back(v.get<unsigned>()); }
