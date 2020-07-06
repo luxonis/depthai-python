4d41b3505d15fbbbabaad1e34d7b487e070b4a7a
Jon Ngai <jon@luxonis.com>
2020-07-06 14:55:53 -0600

    Adding support for big endian systems.

--- host/core/pipeline/cnn_host_pipeline.cpp
+++ host/core/pipeline/cnn_host_pipeline.cpp
@@ -29,6 +29,55 @@ std::list<std::shared_ptr<NNetPacket>> CNNHostPipeline::getConsumedNNetPackets()
 }
 
 
+const int TRANS_INT_SIZE = 4;
+const int TRANS_FLOAT_SIZE = 4;
+const int TRANS_DOUBLE_SIZE = 8;
+
+const int PT_SIZE = 2;
+const int RECT_SIZE = 4;
+
+int isLittleEndian(){
+    int x = 1;
+    return (int) (((char *)&x)[0]);
+}
+
+void read4bytes(unsigned char * output, unsigned char * input){
+    if( isLittleEndian() ){
+        output[0] = input[0];
+        output[1] = input[1];
+        output[2] = input[2];
+        output[3] = input[3];
+    } else {
+        output[0] = input[3];
+        output[1] = input[2];
+        output[2] = input[1];
+        output[3] = input[0];
+    }
+}
+
+void read8bytes(unsigned char * output, unsigned char * input){
+    if( isLittleEndian() ){
+        output[0] = input[0];
+        output[1] = input[1];
+        output[2] = input[2];
+        output[3] = input[3];
+        output[4] = input[4];
+        output[5] = input[5];
+        output[6] = input[6];
+        output[7] = input[7];
+    } else {
+        output[0] = input[7];
+        output[1] = input[6];
+        output[2] = input[5];
+        output[3] = input[4];
+        output[4] = input[3];
+        output[5] = input[2];
+        output[6] = input[1];
+        output[7] = input[0];
+    }
+}
+
+// data being sent by depthai is little endian, we'll need to convert it if the host is expecting the opposite.
 std::list<AprilInfo> CNNHostPipeline::getConsumedAprilPackets()
 {
     std::list<std::shared_ptr<HostDataPacket>> result;
@@ -47,47 +96,47 @@ std::list<AprilInfo> CNNHostPipeline::getConsumedAprilPackets()
             std::vector<AprilDetection> packetDetections;
 
             // create a zarray to access data before packing it into our output type
-// asdfasdf TODO: we're making some assumptions about type sizes and endian. Do we care to handle it and how do we want to do that?
-            memcpy(&aprilInfo.el_sz, &packet->data[headerOffset], sizeof(int));
-            headerOffset = headerOffset + sizeof(int);
+            read4bytes( (unsigned char*) &aprilInfo.el_sz, &packet->data[headerOffset]);
+            headerOffset = headerOffset + TRANS_INT_SIZE;
 
-            memcpy(&aprilInfo.size, &packet->data[headerOffset], sizeof(int));
-            headerOffset = headerOffset + sizeof(int);
+            read4bytes( (unsigned char*) &aprilInfo.size, &packet->data[headerOffset]);
+            headerOffset = headerOffset + TRANS_INT_SIZE;
 
-            memcpy(&aprilInfo.alloc, &packet->data[headerOffset], sizeof(int));
-            headerOffset = headerOffset + sizeof(int);
+            read4bytes( (unsigned char*) &aprilInfo.alloc, &packet->data[headerOffset]);
+            headerOffset = headerOffset + TRANS_INT_SIZE;
 
             for (int i = 0; i < aprilInfo.size; i++) {
-                int dataSize = sizeof(int)+sizeof(int)+sizeof(float)+2*sizeof(double)+4*2*sizeof(double);
+                int dataSize = TRANS_INT_SIZE+TRANS_INT_SIZE+TRANS_FLOAT_SIZE+2*TRANS_DOUBLE_SIZE+4*2*TRANS_DOUBLE_SIZE;
 
                 AprilDetection currDetection;
-                int id;
-                int hamming;
-                float decision_margin;
-                double c[2];
-                double p[4][2];
 
                 unsigned char* dataPtr = (unsigned char*) &packet->data[headerOffset];
 
                 // adding id
-                memcpy(&currDetection.id, dataPtr, sizeof(int));
-                dataPtr = dataPtr + sizeof(int);
+                read4bytes( (unsigned char*) &currDetection.id, dataPtr);
+                dataPtr = dataPtr + TRANS_INT_SIZE;
 
                 // adding hamming (how many error bits were corrected)
-                memcpy(&currDetection.hamming, dataPtr, sizeof(int));
-                dataPtr = dataPtr + sizeof(int);
+                read4bytes( (unsigned char*) &currDetection.hamming, dataPtr);
+                dataPtr = dataPtr + TRANS_INT_SIZE;
 
                 // adding decision margin
-                memcpy(&currDetection.decision_margin, dataPtr, sizeof(float));
-                dataPtr = dataPtr + sizeof(float);
+                read4bytes( (unsigned char*) &currDetection.decision_margin, dataPtr);
+                dataPtr = dataPtr + TRANS_FLOAT_SIZE;
 
                 // adding center
-                memcpy(&currDetection.c[0], dataPtr, 2*sizeof(double));
-                dataPtr = dataPtr + 2*sizeof(double);
+                for(int j=0; j<PT_SIZE; j++){
+                    read8bytes( (unsigned char*) &currDetection.c[j], dataPtr);
+                    dataPtr = dataPtr + TRANS_DOUBLE_SIZE;
+                }
 
                 // reading points for the 4 corners
-                memcpy(&currDetection.p[0][0], dataPtr, 4*2*sizeof(double));
-                dataPtr = dataPtr + 4*2*sizeof(double);
+                for(int j=0; j<RECT_SIZE; j++){
+                    for(int k=0; k<PT_SIZE; k++){
+                        read8bytes( (unsigned char*) &currDetection.p[j][k], dataPtr);
+                        dataPtr = dataPtr + TRANS_DOUBLE_SIZE;
+                    }
+                }
 
                 packetDetections.push_back(currDetection);
 
