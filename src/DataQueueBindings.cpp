ebbade97006d4a47c638b42035ee0c88d04216e7
TheMarpe <martin.peterlin7@gmail.com>
2020-10-19 15:16:35 +0200

    Added bindings for helper layer

--- src/DataQueueBindings.cpp
+++ src/DataQueueBindings.cpp
@@ -18,7 +18,7 @@ void DataQueueBindings::bind(pybind11::module& m){
     py::class_<DataOutputQueue, std::shared_ptr<DataOutputQueue>>(m, "DataOutputQueue")
         .def("get", [](DataOutputQueue& obj){
             
-            std::shared_ptr<RawBuffer> d = nullptr;
+            std::shared_ptr<ADatatype> d = nullptr;
             do {
                 // block for 100ms
                 d = obj.get(milliseconds(100));
@@ -29,12 +29,26 @@ void DataQueueBindings::bind(pybind11::module& m){
             return d;
         })
         .def("has", static_cast<bool(DataOutputQueue::*)()>(&DataOutputQueue::has))
-        .def("tryGet", static_cast<std::shared_ptr<RawBuffer>(DataOutputQueue::*)()>(&DataOutputQueue::tryGet))
+        .def("tryGet", static_cast<std::shared_ptr<ADatatype>(DataOutputQueue::*)()>(&DataOutputQueue::tryGet))
         ;
 
     // Bind DataInputQueue
     py::class_<DataInputQueue, std::shared_ptr<DataInputQueue>>(m, "DataInputQueue")
-        .def("send", [](DataInputQueue& obj, std::shared_ptr<RawBuffer> d){
+        .def("send", [](DataInputQueue& obj, std::shared_ptr<ADatatype> d){
+            
+            bool sent = false;
+            do {
+                // block for 100ms
+                sent = obj.send(d, milliseconds(100));
+
+                // check if interrupt triggered in between
+                if (PyErr_CheckSignals() != 0) throw py::error_already_set();
+
+            } while(!sent);
+
+            return d;
+        })
+        .def("send", [](DataInputQueue& obj, std::shared_ptr<dai::RawBuffer> d){
             
             bool sent = false;
             do {
--- src/DatatypeBindings.cpp
+++ src/DatatypeBindings.cpp
@@ -3,10 +3,16 @@
 #include <unordered_map>
 #include <memory>
 
+// depthai
+#include "depthai/pipeline/datatype/ADatatype.hpp"
+#include "depthai/pipeline/datatype/Buffer.hpp"
+#include "depthai/pipeline/datatype/ImgFrame.hpp"
+#include "depthai/pipeline/datatype/NNData.hpp"
+
 // depthai-shared
 #include "depthai-shared/datatype/RawBuffer.hpp"
-#include "depthai-shared/datatype/ImgFrame.hpp"
-#include "depthai-shared/datatype/NNData.hpp"
+#include "depthai-shared/datatype/RawImgFrame.hpp"
+#include "depthai-shared/datatype/RawNNData.hpp"
 
 
 //pybind
@@ -43,6 +49,10 @@ void DatatypeBindings::bind(pybind11::module& m){
     */
 
     // No copy, opaque std::vector<std::uint8_t> way
+
+
+    // Bind Raw datatypes
+
     //py::bind_vector<std::vector<std::uint8_t>>(m, "VectorByte");
     py::class_<RawBuffer, std::shared_ptr<RawBuffer>>(m, "RawBuffer")
         .def(py::init<>())
@@ -52,18 +62,18 @@ void DatatypeBindings::bind(pybind11::module& m){
 
 
     // Bind ImgFrame
-    py::class_<ImgFrame, RawBuffer, std::shared_ptr<ImgFrame>> imgFrame(m, "ImgFrame");
-    imgFrame
-        .def_readwrite("fb", &ImgFrame::fb)
-        .def_readwrite("category", &ImgFrame::category)
-        .def_readwrite("instanceNum", &ImgFrame::instanceNum)
-        .def_readwrite("sequenceNum", &ImgFrame::sequenceNum)
+    py::class_<RawImgFrame, RawBuffer, std::shared_ptr<RawImgFrame>> rawImgFrame(m, "RawImgFrame");
+    rawImgFrame
+        .def_readwrite("fb", &RawImgFrame::fb)
+        .def_readwrite("category", &RawImgFrame::category)
+        .def_readwrite("instanceNum", &RawImgFrame::instanceNum)
+        .def_readwrite("sequenceNum", &RawImgFrame::sequenceNum)
         .def_property("ts",
-            [](const ImgFrame& o){ 
+            [](const RawImgFrame& o){ 
                 double ts = o.ts.sec + o.ts.nsec / 1000000000.0; 
                 return ts; 
             },
-            [](ImgFrame& o, double ts){ 
+            [](RawImgFrame& o, double ts){ 
                 o.ts.sec = ts; 
                 o.ts.nsec = (ts - o.ts.sec) * 1000000000.0;   
             }  
@@ -71,52 +81,52 @@ void DatatypeBindings::bind(pybind11::module& m){
         ;
 
 
-    py::enum_<ImgFrame::Type>(imgFrame, "Type")
-        .value("YUV422i", ImgFrame::Type::YUV422i)
-        .value("YUV444p", ImgFrame::Type::YUV444p)
-        .value("YUV420p", ImgFrame::Type::YUV420p)
-        .value("YUV422p", ImgFrame::Type::YUV422p)
-        .value("YUV400p", ImgFrame::Type::YUV400p)
-        .value("RGBA8888", ImgFrame::Type::RGBA8888)
-        .value("RGB161616", ImgFrame::Type::RGB161616)
-        .value("RGB888", ImgFrame::Type::RGB888)
-        .value("LUT2", ImgFrame::Type::LUT2)
-        .value("LUT4", ImgFrame::Type::LUT4)
-        .value("LUT16", ImgFrame::Type::LUT16)
-        .value("RAW16", ImgFrame::Type::RAW16)
-        .value("RAW14", ImgFrame::Type::RAW14)
-        .value("RAW12", ImgFrame::Type::RAW12)
-        .value("RAW10", ImgFrame::Type::RAW10)
-        .value("RAW8", ImgFrame::Type::RAW8)
-        .value("PACK10", ImgFrame::Type::PACK10)
-        .value("PACK12", ImgFrame::Type::PACK12)
-        .value("YUV444i", ImgFrame::Type::YUV444i)
-        .value("NV12", ImgFrame::Type::NV12)
-        .value("NV21", ImgFrame::Type::NV21)
-        .value("BITSTREAM", ImgFrame::Type::BITSTREAM)
-        .value("HDR", ImgFrame::Type::HDR)
-        .value("NONE", ImgFrame::Type::NONE)
+    py::enum_<RawImgFrame::Type>(rawImgFrame, "Type")
+        .value("YUV422i", RawImgFrame::Type::YUV422i)
+        .value("YUV444p", RawImgFrame::Type::YUV444p)
+        .value("YUV420p", RawImgFrame::Type::YUV420p)
+        .value("YUV422p", RawImgFrame::Type::YUV422p)
+        .value("YUV400p", RawImgFrame::Type::YUV400p)
+        .value("RGBA8888", RawImgFrame::Type::RGBA8888)
+        .value("RGB161616", RawImgFrame::Type::RGB161616)
+        .value("RGB888", RawImgFrame::Type::RGB888)
+        .value("LUT2", RawImgFrame::Type::LUT2)
+        .value("LUT4", RawImgFrame::Type::LUT4)
+        .value("LUT16", RawImgFrame::Type::LUT16)
+        .value("RAW16", RawImgFrame::Type::RAW16)
+        .value("RAW14", RawImgFrame::Type::RAW14)
+        .value("RAW12", RawImgFrame::Type::RAW12)
+        .value("RAW10", RawImgFrame::Type::RAW10)
+        .value("RAW8", RawImgFrame::Type::RAW8)
+        .value("PACK10", RawImgFrame::Type::PACK10)
+        .value("PACK12", RawImgFrame::Type::PACK12)
+        .value("YUV444i", RawImgFrame::Type::YUV444i)
+        .value("NV12", RawImgFrame::Type::NV12)
+        .value("NV21", RawImgFrame::Type::NV21)
+        .value("BITSTREAM", RawImgFrame::Type::BITSTREAM)
+        .value("HDR", RawImgFrame::Type::HDR)
+        .value("NONE", RawImgFrame::Type::NONE)
         ;
 
-    py::class_<ImgFrame::Specs>(imgFrame, "Specs")
-        .def_readwrite("type", &ImgFrame::Specs::type)
-        .def_readwrite("width", &ImgFrame::Specs::width)
-        .def_readwrite("height", &ImgFrame::Specs::height)
-        .def_readwrite("stride", &ImgFrame::Specs::stride)
-        .def_readwrite("bytesPP", &ImgFrame::Specs::bytesPP)
-        .def_readwrite("p1Offset", &ImgFrame::Specs::p1Offset)
-        .def_readwrite("p2Offset", &ImgFrame::Specs::p2Offset)
-        .def_readwrite("p3Offset", &ImgFrame::Specs::p3Offset)
+    py::class_<RawImgFrame::Specs>(rawImgFrame, "Specs")
+        .def_readwrite("type", &RawImgFrame::Specs::type)
+        .def_readwrite("width", &RawImgFrame::Specs::width)
+        .def_readwrite("height", &RawImgFrame::Specs::height)
+        .def_readwrite("stride", &RawImgFrame::Specs::stride)
+        .def_readwrite("bytesPP", &RawImgFrame::Specs::bytesPP)
+        .def_readwrite("p1Offset", &RawImgFrame::Specs::p1Offset)
+        .def_readwrite("p2Offset", &RawImgFrame::Specs::p2Offset)
+        .def_readwrite("p3Offset", &RawImgFrame::Specs::p3Offset)
         ;   
 
 
 
     // NNData
-    py::class_<NNData, RawBuffer, std::shared_ptr<NNData>> nnData(m, "NNData");
-    nnData
+    py::class_<RawNNData, RawBuffer, std::shared_ptr<RawNNData>> rawNnData(m, "RawNNData");
+    rawNnData
         .def(py::init<>())
-        .def_readwrite("tensors", &NNData::tensors)
-        .def_readwrite("batchSize", &NNData::batchSize)
+        .def_readwrite("tensors", &RawNNData::tensors)
+        .def_readwrite("batchSize", &RawNNData::batchSize)
         ;
 
     py::class_<TensorInfo> tensorInfo(m, "TensorInfo");
@@ -155,6 +165,59 @@ void DatatypeBindings::bind(pybind11::module& m){
         .value("H", TensorInfo::StorageOrder::H)
         .value("W", TensorInfo::StorageOrder::W)
         ;
+
+
+    // Bind non-raw 'helper' datatypes
+    py::class_<ADatatype, std::shared_ptr<ADatatype>>(m, "ADatatype")
+        .def("getRaw", &ADatatype::getRaw);
+
+    py::class_<Buffer, ADatatype, std::shared_ptr<Buffer>>(m, "Buffer")
+        .def(py::init<>())
+        .def("getData", &Buffer::getData)
+        .def("setData", &Buffer::setData)
+        ;
+
+    // Bind ImgFrame
+    py::class_<ImgFrame, Buffer, std::shared_ptr<ImgFrame>>(m, "ImgFrame")
+        .def(py::init<>())
+        // getters
+        .def("getTimestamp", &ImgFrame::getTimestamp)
+        .def("getInstanceNum", &ImgFrame::getInstanceNum)
+        .def("getCategory", &ImgFrame::getCategory)
+        .def("getSequenceNum", &ImgFrame::getSequenceNum)
+        .def("getWidth", &ImgFrame::getWidth)
+        .def("getHeight", &ImgFrame::getHeight)
+        .def("getType", &ImgFrame::getType)
+
+        // setters
+        .def("setTimestamp", &ImgFrame::setTimestamp)
+        .def("setInstanceNum", &ImgFrame::setInstanceNum)
+        .def("setCategory", &ImgFrame::setCategory)
+        .def("setSequenceNum", &ImgFrame::setSequenceNum)
+        .def("setWidth", &ImgFrame::setWidth)
+        .def("setHeight", &ImgFrame::setHeight)
+        .def("setType", &ImgFrame::setType)
+        ;
+
+    // Bind NNData
+    py::class_<NNData, Buffer, std::shared_ptr<NNData>>(m, "NNData")
+        .def(py::init<>())
+        // setters
+        .def("setLayer", py::overload_cast<const std::string&, std::vector<std::uint8_t>>(&NNData::setLayer))
+        .def("setLayer", py::overload_cast<const std::string&, const std::vector<int>&>(&NNData::setLayer))
+        .def("setLayer", py::overload_cast<const std::string&, std::vector<float>>(&NNData::setLayer))
+        .def("setLayer", py::overload_cast<const std::string&, std::vector<double>>(&NNData::setLayer))
+        .def("getLayer", &NNData::getLayer)
+        .def("hasLayer", &NNData::hasLayer)
+        .def("getLayerDatatype", &NNData::getLayerDatatype)
+        .def("getLayerUInt8", &NNData::getLayerUInt8)
+        .def("getLayerFp16", &NNData::getLayerFp16)
+        .def("getFirstLayerUInt8", &NNData::getFirstLayerUInt8)
+        .def("getFirstLayerFp16", &NNData::getFirstLayerFp16)
+        ;
+    
+
+
 
 
 }
