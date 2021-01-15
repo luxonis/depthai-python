53f6919880e112cc8beee55e00d66f3f0da74284
Łukasz Piłatowski <lukpila29@gmail.com>
2021-01-15 16:17:28 +0100

    change cvtColor param

--- examples/06_rgb_full_resolution_saver.py
+++ examples/06_rgb_full_resolution_saver.py
@@ -31,13 +31,13 @@ Path('06_data').mkdir(parents=True, exist_ok=True)
 
 while True:
     in_rgb = q_rgb.get()  # blocking call, will wait until a new data has arrived
-    # data is originally represented as a flat 1D array, it needs to be converted into HxWxC form
+    # data is originally represented as a flat 1D array, it needs to be converted into HxW form
     shape = (in_rgb.getHeight() * 3 // 2, in_rgb.getWidth())
-    frame_rgb = cv2.cvtColor(in_rgb.getData().reshape(shape), cv2.COLOR_YUV2BGR_IYUV)
+    frame_rgb = cv2.cvtColor(in_rgb.getData().reshape(shape), cv2.COLOR_YUV2BGR_NV12)
     # frame is transformed and ready to be shown
     cv2.imshow("rgb", frame_rgb)
     # after showing the frame, it's being stored inside a target directory as a PNG image
-    # cv2.imwrite(f"06_data/{int(time.time() * 10000)}.png", frame_rgb)
+    cv2.imwrite(f"06_data/{int(time.time() * 10000)}.png", frame_rgb)
 
     if cv2.waitKey(1) == ord('q'):
         break
