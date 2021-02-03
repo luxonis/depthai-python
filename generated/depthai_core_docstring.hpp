c1a093e3bd137119b7ff8a91cc22ce73199bf2df
TheMarpe <martin.peterlin7@gmail.com>
2021-02-03 19:06:31 +0100

    Removed depthai_core_docstring.hpp from index

--- generated/depthai_core_docstring.hpp
+++
@@ -1,4 +0,0 @@
-// Default DOC define, which is nullptr aka no docstring
-// This case covers when docstrings couldn't be generated
-// Upon succesful mkdoc invoication, this file will be overwritten with approapriate docstrings
-#define DOC(...) ((const char*) nullptr)
