a508c463d6d51ab414ba9a2e6dbabe773deb3451
TheMarpe <martin.peterlin7@gmail.com>
2021-02-09 00:51:24 +0100

    WIP: RTD configuration

--- docs/readthedocs/ci.cmake
+++ docs/readthedocs/ci.cmake
@@ -1,4 +1,8 @@
 # Builds library at 'build/' and prepares sphinx configuration at 'build/docs/conf.py'
 set(project_root "${CMAKE_CURRENT_LIST_DIR}/../..")
+
+# Print PATH environment variable
+message(STATUS "ENV PATH = $ENV{PATH}")
+
 execute_process(COMMAND ${CMAKE_COMMAND} -D DEPTHAI_PYTHON_BUILD_DOCS:BOOL=YES -D DEPTHAI_BUILD_DOCS:BOOL=YES -S . -B build WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
 execute_process(COMMAND ${CMAKE_COMMAND} --build build --target depthai WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
--- docs/readthedocs/conf.py
+++ docs/readthedocs/conf.py
@@ -1,10 +1,9 @@
 # readthedocs CI build limitation circumvent
 import subprocess
 import os
-import pathlib
 
 # Get current working directory
-cwd = str(pathlib.Path().absolute())
+cwd = os.path.dirname(os.path.realpath(__file__))
 
 # Prepare dependencies for library
 subprocess.check_call(['wget', 'https://github.com/libusb/libusb/releases/download/v1.0.24/libusb-1.0.24.tar.bz2'])
@@ -13,12 +12,11 @@ subprocess.check_call(['./configure', '--disable-udev', '--prefix', cwd+'/../lib
 
 # Add libusb to path
 env=os.environ.copy()
-env['PATH'] = os.environ['PATH']+':'+cwd+'/libusb/include:'+cwd+'/libusb/lib'
-print('Path set to: ', os.environ['PATH'])
+env['PATH'] = env['PATH']+':'+cwd+'/libusb/include:'+cwd+'/libusb/lib'
+print('Path set to: ', env['PATH'])
 
 # Build library and generate actual conf.py
-current_dir = os.path.dirname(os.path.realpath(__file__))
-subprocess.run(["cmake -P ci.cmake"], cwd=current_dir, shell=True, check=True, env=env)
+subprocess.run(["cmake -P ci.cmake"], cwd=cwd, shell=True, check=True, env=env)
 
 # Load generated conf.py which includes all needed information to build with sphinx
 with open("../../build/docs/conf.py") as infile:
