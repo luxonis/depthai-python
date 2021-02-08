f7d274f86429072de0fb1f365c3d1eb8266347c6
TheMarpe <martin.peterlin7@gmail.com>
2021-02-09 00:20:45 +0100

    WIP: Modified mkdoc dependencies

--- docs/readthedocs/conf.py
+++ docs/readthedocs/conf.py
@@ -2,6 +2,14 @@
 import subprocess
 import os
 
+# Prepare dependencies for library
+subprocess.check_call(['wget', 'https://github.com/libusb/libusb/releases/download/v1.0.24/libusb-1.0.24.tar.bz2'])
+subprocess.check_call(['tar', 'xf', 'libusb-1.0.24.tar.bz2'])
+subprocess.check_call(['./configure', '--disable-udev', '--prefix', pwd+'/../libusb'], cwd='libusb-1.0.24')
+# Add libusb to path
+os.environ['PATH'] = os.environ['PATH']+':'+pwd+'/libusb/include:'+pwd+'/libusb/lib'
+print('Path set to: ', os.environ['PATH'])
+
 # Build library and generate actual conf.py
 current_dir = os.path.dirname(os.path.realpath(__file__))
 subprocess.run(["cmake -P ci.cmake"], cwd=current_dir, shell=True, check=True)
