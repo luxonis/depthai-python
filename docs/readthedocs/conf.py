f7df0d54e6aab0ed9fb15a68e470d94921f40ff2
TheMarpe <martin.peterlin7@gmail.com>
2021-02-09 00:34:38 +0100

    WIP: RTD configuration

--- docs/readthedocs/conf.py
+++ docs/readthedocs/conf.py
@@ -12,12 +12,13 @@ subprocess.check_call(['tar', 'xf', 'libusb-1.0.24.tar.bz2'])
 subprocess.check_call(['./configure', '--disable-udev', '--prefix', cwd+'/../libusb'], cwd='libusb-1.0.24')
 
 # Add libusb to path
-os.environ['PATH'] = os.environ['PATH']+':'+cwd+'/libusb/include:'+cwd+'/libusb/lib'
+env=os.environ.copy()
+env['PATH'] = os.environ['PATH']+':'+cwd+'/libusb/include:'+cwd+'/libusb/lib'
 print('Path set to: ', os.environ['PATH'])
 
 # Build library and generate actual conf.py
 current_dir = os.path.dirname(os.path.realpath(__file__))
-subprocess.run(["cmake -P ci.cmake"], cwd=current_dir, shell=True, check=True)
+subprocess.run(["cmake -P ci.cmake"], cwd=current_dir, shell=True, check=True, env=env)
 
 # Load generated conf.py which includes all needed information to build with sphinx
 with open("../../build/docs/conf.py") as infile:
