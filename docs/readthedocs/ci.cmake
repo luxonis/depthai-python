474c79d7d2bfa8c4068a215411ff381a07d68b25
TheMarpe <martin.peterlin7@gmail.com>
2021-02-09 21:51:34 +0100

    WIP: RTD - source directory

--- docs/readthedocs/ci.cmake
+++ docs/readthedocs/ci.cmake
@@ -1,9 +1,8 @@
 # Builds library at 'build/' and prepares sphinx configuration at 'build/docs/conf.py'
 set(project_root "${CMAKE_CURRENT_LIST_DIR}/../..")
 
-# Copy docs/source folder to docs/readthedocs
-file(GLOB to_copy "${project_root}/docs/source/*" LIST_DIRECTORIES true)
-file(COPY ${to_copy} DESTINATION "${project_root}/docs/readthesource/")
+# Copy docs/source/ contents to docs/readthedocs/ folder
+file(COPY "${project_root}/docs/source/" DESTINATION ${CMAKE_CURRENT_LIST_DIR})
 
 execute_process(COMMAND ${CMAKE_COMMAND} -D DEPTHAI_PYTHON_BUILD_DOCS:BOOL=YES -D DEPTHAI_BUILD_DOCS:BOOL=YES -S . -B build WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
 execute_process(COMMAND ${CMAKE_COMMAND} --build build --target depthai WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
