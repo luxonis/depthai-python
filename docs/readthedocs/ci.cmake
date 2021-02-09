965fcd0f45ec0029285146492f03439e488b67e5
TheMarpe <martin.peterlin7@gmail.com>
2021-02-09 22:42:20 +0100

    WIP: RTD - build all

--- docs/readthedocs/ci.cmake
+++ docs/readthedocs/ci.cmake
@@ -5,4 +5,4 @@ set(project_root "${CMAKE_CURRENT_LIST_DIR}/../..")
 file(COPY "${project_root}/docs/source/" DESTINATION ${CMAKE_CURRENT_LIST_DIR})
 
 execute_process(COMMAND ${CMAKE_COMMAND} -D DEPTHAI_PYTHON_BUILD_DOCS:BOOL=YES -D DEPTHAI_BUILD_DOCS:BOOL=YES -S . -B build WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
-execute_process(COMMAND ${CMAKE_COMMAND} --build build --target depthai WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
+execute_process(COMMAND ${CMAKE_COMMAND} --build build --parallel WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
