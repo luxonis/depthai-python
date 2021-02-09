70749ed501716751b1c1da3bd8c5bc338c18d498
TheMarpe <martin.peterlin7@gmail.com>
2021-02-09 01:24:00 +0100

    WIP: RTD configuration

--- docs/readthedocs/conf.py
+++ docs/readthedocs/conf.py
@@ -9,6 +9,8 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 subprocess.check_call(['wget', 'https://github.com/libusb/libusb/releases/download/v1.0.24/libusb-1.0.24.tar.bz2'])
 subprocess.check_call(['tar', 'xf', 'libusb-1.0.24.tar.bz2'])
 subprocess.check_call(['./configure', '--disable-udev', '--prefix', cwd+'/../libusb'], cwd='libusb-1.0.24')
+subprocess.check_call(['make', '-j'], cwd='libusb-1.0.24')
+subprocess.check_call(['make', 'intall'], cwd='libusb-1.0.24')
 
 # Add libusb to path
 env=os.environ.copy()
