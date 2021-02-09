54ed95e0c0021825955668189e76d64e65a4ef15
TheMarpe <martin.peterlin7@gmail.com>
2021-02-09 21:40:10 +0100

    WIP: RTD - source directory

--- docs/readthedocs/ci.cmake
+++ docs/readthedocs/ci.cmake
@@ -2,7 +2,8 @@
 set(project_root "${CMAKE_CURRENT_LIST_DIR}/../..")
 
 # Copy docs/source folder to docs/readthedocs
-file(COPY "${project_root}/docs/source" DESTINATION "${project_root}/docs/readthesource")
+file(GLOB to_copy "${project_root}/docs/source/*" LIST_DIRECTORIES true)
+file(COPY ${to_copy} DESTINATION "${project_root}/docs/readthesource/")
 
 execute_process(COMMAND ${CMAKE_COMMAND} -D DEPTHAI_PYTHON_BUILD_DOCS:BOOL=YES -D DEPTHAI_BUILD_DOCS:BOOL=YES -S . -B build WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
 execute_process(COMMAND ${CMAKE_COMMAND} --build build --target depthai WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
