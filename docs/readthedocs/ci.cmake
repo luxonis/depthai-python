f1ae66aeed4181dfc5a8aadf2473626ab7b66c5f
TheMarpe <martin.peterlin7@gmail.com>
2021-02-09 16:59:22 +0100

    WIP: RTD - Added libclang for pybind11_mkdoc

--- docs/readthedocs/ci.cmake
+++ docs/readthedocs/ci.cmake
@@ -1,8 +1,8 @@
 # Builds library at 'build/' and prepares sphinx configuration at 'build/docs/conf.py'
 set(project_root "${CMAKE_CURRENT_LIST_DIR}/../..")
 
-# Print PATH environment variable
-message(STATUS "ENV PATH = $ENV{PATH}")
+# Copy docs/source folder to docs/readthedocs
+file(COPY "${project_root}/docs/source" DESTINATION "${project_root}/docs/readthesource")
 
 execute_process(COMMAND ${CMAKE_COMMAND} -D DEPTHAI_PYTHON_BUILD_DOCS:BOOL=YES -D DEPTHAI_BUILD_DOCS:BOOL=YES -S . -B build WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
 execute_process(COMMAND ${CMAKE_COMMAND} --build build --target depthai WORKING_DIRECTORY ${project_root} COMMAND_ECHO STDOUT)
--- docs/readthedocs/conf.py
+++ docs/readthedocs/conf.py
@@ -6,16 +6,20 @@ import os
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 # Prepare dependencies for library
+env=os.environ.copy()
+
+# Libusb
 subprocess.check_call(['wget', 'https://github.com/libusb/libusb/releases/download/v1.0.24/libusb-1.0.24.tar.bz2'])
 subprocess.check_call(['tar', 'xf', 'libusb-1.0.24.tar.bz2'])
 subprocess.check_call(['./configure', '--disable-udev', '--prefix', cwd+'/libusb'], cwd='libusb-1.0.24')
 subprocess.check_call(['make', '-j'], cwd='libusb-1.0.24')
 subprocess.check_call(['make', 'install'], cwd='libusb-1.0.24')
-
-# Add libusb to path
-env=os.environ.copy()
 env['PATH'] = cwd+'/libusb/include:'+cwd+'/libusb/lib'+':'+env['PATH']
-print('Path set to: ', env['PATH'])
+
+# libclang
+subprocess.check_call(['wget', 'https://artifacts.luxonis.com/artifactory/luxonis-depthai-data-local/misc/libclang-11_manylinux2014_x86_64.tar.xz'])
+subprocess.check_call(['tar', 'xf', 'libclang-11_manylinux2014_x86_64.tar.xz', '-C', cwd+'/libclang/'])
+env['LIBCLANG_PATH'] = cwd+'/libclang/usr/lib/llvm-11/lib/libclang.so.1'
 
 # Build library and generate actual conf.py
 subprocess.run(["cmake -P ci.cmake"], cwd=cwd, shell=True, check=True, env=env)
