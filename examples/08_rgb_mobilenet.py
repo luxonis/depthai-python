49e055ebb9ba787674188511ff74f46755696198
Łukasz Piłatowski <lukpila29@gmail.com>
2021-03-03 22:24:34 +0100

    migrate changes from 21_mobilenet_device_side_decoding.py

--- examples/08_rgb_mobilenet.py
+++ examples/08_rgb_mobilenet.py
@@ -6,11 +6,12 @@ import cv2
 import depthai as dai
 import numpy as np
 import time
+import argparse
 
-# Get argument first
-mobilenet_path = str((Path(__file__).parent / Path('models/mobilenet.blob')).resolve().absolute())
-if len(sys.argv) > 1:
-    mobilenet_path = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument('mobilenet_path', nargs='?', help="Path to mobilenet detection network blob", default=str((Path(__file__).parent / Path('models/mobilenet.blob')).resolve().absolute()))
+parser.add_argument('-s', '--sync', action="store_true", help="Sync RGB output with NN output", default=False)
+args = parser.parse_args()
 
 # Start defining a pipeline
 pipeline = dai.Pipeline()
@@ -19,17 +20,23 @@ pipeline = dai.Pipeline()
 cam_rgb = pipeline.createColorCamera()
 cam_rgb.setPreviewSize(300, 300)
 cam_rgb.setInterleaved(False)
+cam_rgb.setFps(40)
 
 # Define a neural network that will make predictions based on the source frames
 detection_nn = pipeline.createMobileNetDetectionNetwork()
 detection_nn.setConfidenceThreshold(0.5)
-detection_nn.setBlobPath(mobilenet_path)
+detection_nn.setBlobPath(args.mobilenet_path)
+detection_nn.setNumInferenceThreads(2)
+detection_nn.input.setBlocking(False)
 cam_rgb.preview.link(detection_nn.input)
 
 # Create outputs
 xout_rgb = pipeline.createXLinkOut()
 xout_rgb.setStreamName("rgb")
-cam_rgb.preview.link(xout_rgb.input)
+if(args.sync):
+    detection_nn.passthrough.link(xout_rgb.input)
+else:
+    cam_rgb.preview.link(xout_rgb.input)
 
 xout_nn = pipeline.createXLinkOut()
 xout_nn.setStreamName("nn")
@@ -51,6 +58,8 @@ with dai.Device(pipeline) as device:
 
     start_time = time.time()
     counter = 0
+    detections = []
+    frame = None
 
     # nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
     def frame_norm(frame, bbox):
@@ -60,22 +69,32 @@ with dai.Device(pipeline) as device:
 
 
     while True:
-        # use blocking get() call to catch frame and inference result synced
-        in_rgb = q_rgb.get()
-        in_nn = q_nn.get()
+        if(args.sync):
+            # use blocking get() call to catch frame and inference result synced
+            in_rgb = q_rgb.get()
+            in_nn = q_nn.get()
+        else:
+            # instead of get (blocking) used tryGet (nonblocking) which will return the available data or None otherwise
+            in_rgb = q_rgb.tryGet()
+            in_nn = q_nn.tryGet()
 
-        frame = in_rgb.getCvFrame()
-        counter += 1
+        if in_rgb is not None:
+            frame = in_rgb.getCvFrame()
+            cv2.putText(frame, "NN fps: {:.2f}".format(counter / (time.time() - start_time)), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.4, color=(255, 255, 255))
+
+        if in_nn is not None:
+            detections = in_nn.detections
+            counter += 1
 
         # if the frame is available, draw bounding boxes on it and show the frame
-        for detection in in_nn.detections:
-            bbox = frame_norm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
-            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-            cv2.putText(frame, texts[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, f"{int(detection.confidence*100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, "NN fps: {:.2f}".format(counter / (time.time() - start_time)), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color = (255, 255, 255))
-        
-        cv2.imshow("rgb", frame)
+        if frame is not None:
+            for detection in detections:
+                bbox = frame_norm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+                cv2.putText(frame, texts[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                cv2.putText(frame, f"{int(detection.confidence*100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+
+            cv2.imshow("rgb", frame)
 
         if cv2.waitKey(1) == ord('q'):
             break
