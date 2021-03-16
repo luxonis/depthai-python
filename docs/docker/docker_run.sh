453a5d9cfb50dfb32f6350c4a19a902d6ecbc687
Łukasz Piłatowski <lukpila29@gmail.com>
2021-03-16 13:58:06 +0100

    fix docker build & update

--- docs/docker/docker_run.sh
+++ docs/docker/docker_run.sh
@@ -4,6 +4,6 @@ set -e
 
 ./docs/docker/update_docs.sh
 
-when-changed -r docs/source -c "bash /app/docs/update_docs.sh" &
+when-changed -r docs/source -c "bash /app/docs/docker/update_docs.sh" &
 
 python3 -m http.server --bind 0.0.0.0 8000 --directory build/docs/sphinx
--- docs/docker/update_docs.sh
+++ docs/docker/update_docs.sh
@@ -5,3 +5,6 @@ set -e
 rm -rf build/docs/sphinx
 cmake -S . -B build -D DEPTHAI_BUILD_DOCS=ON -D DEPTHAI_PYTHON_BUILD_DOCS=ON -D ENABLE_PRECOMPILED_HEADERS=OFF
 cmake --build build --parallel --target sphinx
+
+# fix missing index.css file
+cp /app/docs/source/_static/css/* /app/build/docs/sphinx/_static/css
