Fix scaling issue

examples/03_depth_preview.py
@@ -41,10 +41,8 @@ with dai.Device(pipeline) as device:
 
     while True:
         inDepth = q.get()  # blocking call, will wait until a new data has arrived
-        # data is originally represented as a flat 1D array, it needs to be converted into HxW form
-        frame = inDepth.getData().reshape((inDepth.getHeight(), inDepth.getWidth())).astype(np.uint8)
-        frame = np.ascontiguousarray(frame)
-        # frame is transformed, the color map will be applied to highlight the depth info
+        frame = inDepth.getFrame()
+        frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
         frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
 
         # Uncomment one of these and comment the one given above

examples/10_mono_depth_mobilenetssd.py
@@ -53,7 +53,7 @@ manip.out.link(nn.input)
 depthOut = pipeline.createXLinkOut()
 depthOut.setStreamName("depth")
 
-stereo.depth.link(depthOut.input)
+stereo.disparity.link(depthOut.input)
 
 xoutRight = pipeline.createXLinkOut()
 xoutRight.setStreamName("rectifiedRight")
@@ -109,10 +109,37 @@ with dai.Device(pipeline) as device:
             detections = inDet.detections
 
         if inDepth is not None:
-            depthFrame = inDepth.getFrame()
-            depthFrame = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
-            depthFrame = cv2.equalizeHist(depthFrame)
-            depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_HOT)
+            # depthFrame = inDepth.getFrame()
+            # data is originally represented as a flat 1D array, it needs to be converted into HxW form
+            depthFrame = inDepth.getData().reshape((inDepth.getHeight(), inDepth.getWidth())).astype(np.uint8)
+            depthFrame = np.ascontiguousarray(depthFrame)
+            # frame is transformed, the color map will be applied to highlight the depth info
+            depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_JET)
+
+            # Uncomment one of these and comment the one given above
+            # to see visualisation in different color frames
+
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_BONE)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_AUTUMN)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_WINTER)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_RAINBOW)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_OCEAN)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_SUMMER)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_SPRING)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_COOL)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_HSV)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_HOT)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_PINK)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_PARULA)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_MAGMA)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_INFERNO)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_PLASMA)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_VIRIDIS)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_CIVIDIS)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_TWILIGHT)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_TWILIGHT_SHIFTED)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_TURBO)
+            # depthFrame = cv2.applyColorMap(depthFrame, cv2.COLORMAP_DEEPGREEN)
 
         if rightFrame is not None:
             displayFrame("rectified right", rightFrame)

examples/11_rgb_encoding_mono_mobilenet.py
@@ -74,21 +74,16 @@ with dai.Device(pipeline) as device:
     frame = None
     frameManip = None
     detections = []
+    offset_x = (camRight.getResolutionWidth() - camRight.getResolutionHeight()) // 2
 
     def frameNorm(frame, bbox):
         normVals = np.full(len(bbox), frame.shape[0])
         normVals[::2] = frame.shape[1]
         return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
 
-    def displayFrame(name, frame):
-        for detection in detections:
-            bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
-            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-            cv2.putText(frame, labelMap[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-        cv2.imshow(name, frame)
-
     videoFile = open('video.h265', 'wb')
+    cv2.namedWindow("right", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("manip", cv2.WINDOW_NORMAL)
 
     while True:
         inRight = qRight.tryGet()
@@ -108,10 +103,22 @@ with dai.Device(pipeline) as device:
             detections = inDet.detections
 
         if frame is not None:
-            displayFrame("right", frame)
+            cropped_frame = np.zeros((camRight.getResolutionHeight(), camRight.getResolutionHeight()))
+            for detection in detections:
+                bbox = frameNorm(cropped_frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+                bbox[::2] += offset_x
+                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+                cv2.putText(frame, labelMap[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.imshow("right", frame)
 
         if frameManip is not None:
-            displayFrame("manip", frameManip)
+            for detection in detections:
+                bbox = frameNorm(frameManip, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+                cv2.rectangle(frameManip, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+                cv2.putText(frameManip, labelMap[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                cv2.putText(frameManip, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.imshow("manip", frameManip)
 
         if cv2.waitKey(1) == ord('q'):
             break