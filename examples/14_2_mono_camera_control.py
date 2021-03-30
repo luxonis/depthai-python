abc2e077bae99ad8296a12ad4a151bada9df6fdc
CsabaGergely <csaba@luxonis.com>
2021-03-30 17:23:38 +0300

    Add crop movement with WASD

--- examples/14_2_mono_camera_control.py
+++ examples/14_2_mono_camera_control.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3
 
 """
-This example shows usage of Camera Control message as well as MononCamera configInput to change crop x and y
-Uses 'WASD' controls to move the crop window, 'C' to capture a still image, 'T' to trigger autofocus, 'IOKL,.'
-for manual exposure/focus:
-  Control:      key[dec/inc]  min..max
-  exposure time:     I   O      1..33000 [us]
-  sensitivity iso:   K   L    100..1600
-  focus:             ,   .      0..255 [far..near]
-To go back to auto controls:
-  'E' - autoexposure
-  'F' - autofocus (continuous)
+This example shows usage of mono camera in crop mode with the possibility to move the crop.
+Use 'WASD' in order to do it.
 """
 
 
 import cv2
 import depthai as dai
+
+stepSize = 0.02
 
 # Start defining a pipeline
 pipeline = dai.Pipeline()
@@ -24,22 +18,27 @@ pipeline = dai.Pipeline()
 camRight = pipeline.createMonoCamera()
 camRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 camRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
-
 camLeft = pipeline.createMonoCamera()
 camLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
 camLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
 
-# Create a node to convert the grayscale frame into the nn-acceptable form
+
+topLeft = dai.Point2f(0.4, 0.4)
+bottomRight = dai.Point2f(0.6, 0.6)
+
 manipRight = pipeline.createImageManip()
-manipRight.initialConfig.setCropRect(0.4, 0.4, 0.6, 0.6)
-# The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
-manipRight.initialConfig.setFrameType(dai.RawImgFrame.Type.BGR888p)
-
+manipRight.initialConfig.setCropRect(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y)
 manipLeft = pipeline.createImageManip()
-manipLeft.initialConfig.setCropRect(0.4, 0.4, 0.6, 0.6)
-# The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
-manipLeft.initialConfig.setFrameType(dai.RawImgFrame.Type.BGR888p)
+manipLeft.initialConfig.setCropRect(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y)
+manipRight.setMaxOutputFrameSize(camRight.getResolutionHeight()*camRight.getResolutionWidth()*3)
 
+# Control movement
+controlIn = pipeline.createXLinkIn()
+controlIn.setStreamName('control')
+controlIn.out.link(manipRight.inputConfig)
+controlIn.out.link(manipLeft.inputConfig)
+
+# Linking with USB
 camRight.out.link(manipRight.inputImage)
 camLeft.out.link(manipLeft.inputImage)
 
@@ -57,12 +56,15 @@ with dai.Device(pipeline) as device:
     # Start pipeline
     device.startPipeline()
 
-    # Output queues will be used to get the grayscale frames and nn data from the outputs defined above
+    # Output queues will be used to get the grayscale frames
     qRight = device.getOutputQueue(manipOutRight.getStreamName(), maxSize=4, blocking=False)
     qLeft = device.getOutputQueue(manipOutLeft.getStreamName(), maxSize=4, blocking=False)
+    controlQueue = device.getInputQueue(controlIn.getStreamName())
 
     def displayFrame(name, frame):
         cv2.imshow(name, frame)
+
+    sendCamConfig = False
 
     while True:
         inRight = qRight.get()
@@ -72,5 +74,36 @@ with dai.Device(pipeline) as device:
         displayFrame("right", frameRight)
         displayFrame("left", frameLeft)
 
-        if cv2.waitKey(1) == ord('q'):
+   
+        
+        # Update screen
+        key = cv2.waitKey(1)
+        if key == ord('q'):
             break
+        elif key == ord('w'):
+            if topLeft.y - stepSize >= 0:
+                topLeft.y -= stepSize
+                bottomRight.y -= stepSize
+                sendCamConfig = True        
+        elif key == ord('a'):
+            if topLeft.x - stepSize >= 0:
+                topLeft.x -= stepSize
+                bottomRight.x -= stepSize
+                sendCamConfig = True
+        elif key == ord('s'):
+            if bottomRight.y + stepSize <= 1:
+                topLeft.y += stepSize
+                bottomRight.y += stepSize
+                sendCamConfig = True  
+        elif key == ord('d'):
+            if bottomRight.x + stepSize <= 1:
+                topLeft.x += stepSize
+                bottomRight.x += stepSize            
+                sendCamConfig = True
+
+
+        if sendCamConfig:
+            cfg = dai.ImageManipConfig()
+            cfg.setCropRect(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y)
+            controlQueue.send(cfg)
+            sendCamConfig = False                
