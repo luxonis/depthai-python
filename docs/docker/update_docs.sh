dee72d3be344d473d7d1fcd1b911ac3df774517b
Martin Peterlin <martin.peterlin7@gmail.com>
2021-04-01 02:47:16 +0200

    Addresses CMake 3.20 regression in parsing '--parallel' ('-j') option

--- docs/docker/update_docs.sh
+++ docs/docker/update_docs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cmake --build build --parallel --target sphinx
+cmake --build build --target sphinx --parallel
 
 # fix missing index.css file
 cp /app/docs/source/_static/css/* /app/build/docs/sphinx/_static/css
