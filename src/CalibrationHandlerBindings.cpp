e33276ea35de36815a1cfd3edc68a2bab393a8ae
Sachin <sachin.guruswamy@gmail.com>
2021-04-06 19:47:52 -0700

    added data bindings
    Signed-off-by: Sachin <sachin.guruswamy@gmail.com>

---
+++ src/CalibrationHandlerBindings.cpp
@@ -0,0 +1,37 @@
+#include "CalibrationHandlerBindings.hpp"
+#include "depthai/device/CalibrationHandler.hpp"
+
+void PipelineBindings::bind(pybind11::module& m){
+
+    using namespace dai;
+
+    // bind pipeline
+    py::class_<CalibrationHandler>(m, "CalibrationHandler", DOC(dai, CalibrationHandler))
+        .def(py::init<>(), DOC(dai, CalibrationHandler, CalibrationHandler, 1))
+        .def(py::init<std::string>(), DOC(dai, CalibrationHandler, CalibrationHandler, 2))
+        .def(py::init<std::string, std::string>(), DOC(dai, CalibrationHandler, CalibrationHandler, 3))
+        .def("getCameraIntrinsics", &CalibrationHandler::getCameraIntrinsics, py::arg("cameraId"), py::arg("resizeHeight"), py::arg("resizewidth"), py::arg("topLeftPixelId"), py::arg("bottomRightPixelId"), DOC(dai, Device, getCameraIntrinsics))
+        .def("getDefaultIntrinsics", &CalibrationHandler::getDefaultIntrinsics, py::arg("cameraId"), DOC(dai, Device, getDefaultIntrinsics))
+
+        .def("getDistortionCoefficients", &CalibrationHandler::getDistortionCoefficients, py::arg("cameraId"), DOC(dai, Device, getDistortionCoefficients))
+        .def("getCameraExtrinsics", &CalibrationHandler::getCameraExtrinsics, py::arg("srcCamera"), py::arg("dstCamera"), py::arg("useMeasuredTranslation") = false, DOC(dai, Device, getCameraExtrinsics))
+
+        .def("getCameraToImuExtrinsics", &CalibrationHandler::getCameraToImuExtrinsics, py::arg("cameraId"), py::arg("useMeasuredTranslation") = false, DOC(dai, Device, getCameraToImuExtrinsics))
+        .def("getImuToCameraExtrinsics", &CalibrationHandler::getImuToCameraExtrinsics, py::arg("cameraId"), py::arg("useMeasuredTranslation") = false, DOC(dai, Device, getImuToCameraExtrinsics))
+        // TODO(sachin): eepromToJsonFile function. should I add argument ?
+        .def("eepromToJsonFile", &CalibrationHandler::eepromToJsonFile, py::arg("destPath"), DOC(dai, Device, eepromToJsonFile))
+        
+        .def("setBoardInfo", &CalibrationHandler::setBoardInfo, py::arg("version"), py::arg("swapLeftRightCam"), py::arg("boardName"), py::arg("boardRev"), DOC(dai, Device, setBoardInfo))
+        .def("setCameraIntrinsics", &CalibrationHandler::setCameraIntrinsics, py::arg("cameraId"), py::arg("intrinsics"), py::arg("width"), py::arg("height"), DOC(dai, Device, setCameraIntrinsics))
+        .def("setdistortionCoefficients", &CalibrationHandler::setdistortionCoefficients, py::arg("cameraId"), py::arg("distortionCoefficients"), DOC(dai, Device, setdistortionCoefficients))
+        .def("setFov", &CalibrationHandler::setFov, py::arg("cameraId"), py::arg("hfov"), DOC(dai, Device, setFov))
+        .def("setCameraExtrinsics", &CalibrationHandler::setCameraExtrinsics, py::arg("srcCameraId"), py::arg("destCameraId"), py::arg("rotationMatrix"), py::arg("translation"), DOC(dai, Device, setCameraExtrinsics))
+
+        .def("setImuExtrinsics", &CalibrationHandler::setImuExtrinsics, py::arg("destCameraId"), py::arg("rotationMatrix"), py::arg("translation"), DOC(dai, Device, setImuExtrinsics))
+        .def("setStereoLeft", &CalibrationHandler::setStereoLeft, py::arg("cameraId"), py::arg("rectifiedRotation"), DOC(dai, Device, setStereoLeft))
+        .def("setStereoRight", &CalibrationHandler::setStereoRight, py::arg("cameraId"), py::arg("rectifiedRotation"), DOC(dai, Device, setStereoRight))
+
+
+        // .def("getEepromData", &CalibrationHandler::getEepromData, DOC(dai, Device, getEepromData));
+
+}
