566a89140458cac765f1867387fa97cb33b37d2c
Sachin <sachin.guruswamy@gmail.com>
2021-04-07 19:23:08 -0700

    restructuring
    Signed-off-by: Sachin <sachin.guruswamy@gmail.com>

--- src/pipeline/CommonBindings.cpp
+++ src/pipeline/CommonBindings.cpp
@@ -2,6 +2,7 @@
 
 // depthai-shared
 #include "depthai-shared/common/CameraBoardSocket.hpp"
+#include "depthai-shared/common/EepromData.hpp"
 #include "depthai-shared/common/CameraImageOrientation.hpp"
 #include "depthai-shared/common/MemoryInfo.hpp"
 #include "depthai-shared/common/ChipTemperature.hpp"
@@ -52,5 +53,54 @@ void CommonBindings::bind(pybind11::module& m){
         .def_readwrite("average", &CpuUsage::average)
         .def_readwrite("msTime", &CpuUsage::msTime)
     ;
+    // CameraModel enum bindings
+    py::enum_<CameraModel>(m, "CameraModel", DOC(dai, CameraModel))
+        .value("Perspective", CameraModel::Perspective)
+        .value("Fisheye", CameraModel::Fisheye)
+        .value("Equirectangular", CameraModel::Equirectangular)
+        .value("RadialDivision", CameraModel::RadialDivision)
+    ;
 
+    // StereoRectification
+    py::class_<StereoRectification> (m, "StereoRectification", DOC(dai, StereoRectification))
+        .def(py::init<>())
+        .def_readwrite("rectifiedRotationLeft", &StereoRectification::rectifiedRotationLeft)
+        .def_readwrite("rectifiedRotationRight", &StereoRectification::rectifiedRotationRight)
+        .def_readwrite("leftCameraSocket", &StereoRectification::leftCameraSocket)
+        .def_readwrite("rightCameraSocket", &StereoRectification::rightCameraSocket)
+        ;
+
+    // Extrinsics 
+    py::class_<Extrinsics> (m, "Extrinsics", DOC(dai, Extrinsics))
+        .def(py::init<>())
+        .def_readwrite("rotationMatrix", &Extrinsics::rotationMatrix)
+        .def_readwrite("translation", &Extrinsics::translation)
+        .def_readwrite("measuredTranslation", &Extrinsics::measuredTranslation)
+        .def_readwrite("toCameraSocket", &Extrinsics::toCameraSocket)
+        .def_readwrite("fromCameraSocket", &Extrinsics::fromCameraSocket)
+        ;
+
+    // CameraInfo 
+    py::class_<CameraInfo> (m, "CameraInfo", DOC(dai, CameraInfo))
+        .def(py::init<>())
+        .def_readwrite("width", &CameraInfo::width)
+        .def_readwrite("height", &CameraInfo::height)
+        .def_readwrite("intrinsicMatrix", &CameraInfo::intrinsicMatrix)
+        .def_readwrite("distortionCoeff", &CameraInfo::distortionCoeff)
+        .def_readwrite("extrinsics", &CameraInfo::extrinsics)
+        .def_readwrite("cameraType", &CameraInfo::cameraType)
+        .def_readwrite("measuredFovDeg", &CameraInfo::measuredFovDeg)
+        ;
+
+    // EepromData 
+    py::class_<EepromData> (m, "EepromData", DOC(dai, EepromData))
+        .def(py::init<>())
+        .def_readwrite("version", &EepromData::version)
+        .def_readwrite("swapLeftRightCam", &EepromData::swapLeftRightCam)
+        .def_readwrite("boardName", &EepromData::boardName)
+        .def_readwrite("boardRev", &EepromData::boardRev)
+        .def_readwrite("cameraData", &EepromData::cameraData)
+        .def_readwrite("stereoRectificationData", &EepromData::stereoRectificationData)
+        .def_readwrite("imuExtrinsics", &EepromData::imuExtrinsics)
+        ;
 }
