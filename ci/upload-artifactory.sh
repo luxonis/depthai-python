2120e0837ed444157fa67066218dd5c8ff3abdce
SzabolcsGergely <szabi@luxonis.com>
2021-04-12 19:44:37 +0300

    Update deprecated jfrog commands

--- ci/upload-artifactory.sh
+++ ci/upload-artifactory.sh
@@ -5,5 +5,5 @@ curl -fL https://getcli.jfrog.io | sh
 cd wheelhouse/audited/ || exit 1
 export PATH_PREFIX=luxonis-python-snapshot-local/depthai
 
-../../jfrog rt config --url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
+../../jfrog config add --artifactory-url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
 ../../jfrog rt u "*" "$PATH_PREFIX/"
