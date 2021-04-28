f13bac21ad69df05482f5dd3cfcc304f225d778d
SzabolcsGergely <szabi@luxonis.com>
2021-04-29 00:05:00 +0300

    Improve timestamp printing

--- examples/31_1_imu_gyro_accelero.py
+++ examples/31_1_imu_gyro_accelero.py
@@ -14,13 +14,13 @@ xlinkOut = pipeline.createXLinkOut()
 xlinkOut.setStreamName("imu")
 
 sensorConfig = dai.IMUSensorConfig()
-sensorConfig.reportIntervalUs = 2500 # 400hz
+sensorConfig.reportIntervalUs = 2500
 sensorConfig.sensorId = dai.IMUSensorId.IMU_ACCELEROMETER
 imu.enableIMUSensor(sensorConfig)
 sensorConfig.sensorId = dai.IMUSensorId.IMU_GYROSCOPE_CALIBRATED
 imu.enableIMUSensor(sensorConfig)
 # above this threshold packets will be sent in batch of X, if the host is not blocked
-imu.setBatchReportThreshold(5)
+imu.setBatchReportThreshold(1)
 # maximum number of IMU packets in a batch, if it's reached device will block sending until host can receive it
 # if lower or equal to batchReportThreshold then the sending is always blocking on device
 imu.setMaxBatchReports(5)
@@ -35,6 +35,9 @@ with dai.Device(pipeline) as device:
     baseTs = time.monotonic()
     device.startPipeline()
 
+    def timeDeltaToMilliS(delta) -> float:
+        return delta.total_seconds()*1000
+
     # Output queue for imu bulk packets
     imuQueue = device.getOutputQueue(name="imu", maxSize=4, blocking=False)
     baseTs = None
@@ -47,16 +50,17 @@ with dai.Device(pipeline) as device:
             gyroTs = imuData.gyroscope.timestamp.getTimestamp()
             if baseTs is None:
                 baseTs = acceleroTs if acceleroTs < gyroTs else gyroTs
-            acceleroTs = acceleroTs - baseTs
-            gyroTs = gyroTs - baseTs
+            acceleroTs = timeDeltaToMilliS(acceleroTs - baseTs)
+            gyroTs = timeDeltaToMilliS(gyroTs - baseTs)
 
-            ffs = "{: .06f}"
+            imup = "{: .06f}"
+            tsp = "{: .03f}"
             accelLength = math.sqrt(imuData.acceleroMeter.x**2 + imuData.acceleroMeter.y**2 + imuData.acceleroMeter.z**2)
 
-            print(f"Accelero timestamp: {acceleroTs} s")
-            print(f"Accelero: x: {ffs.format(imuData.acceleroMeter.x)} y: {ffs.format(imuData.acceleroMeter.y)} z: {ffs.format(imuData.acceleroMeter.z)}, length {ffs.format(accelLength)}")
-            print(f"Gyro timestamp: {gyroTs} s")
-            print(f"Gyro: x: {ffs.format(imuData.gyroscope.x)} y: {ffs.format(imuData.gyroscope.y)} z: {ffs.format(imuData.gyroscope.z)} ")
+            print(f"Accelero timestamp: {tsp.format(acceleroTs)} ms")
+            print(f"Accelero: x: {imup.format(imuData.acceleroMeter.x)} y: {imup.format(imuData.acceleroMeter.y)} z: {imup.format(imuData.acceleroMeter.z)}, length {imup.format(accelLength)}")
+            print(f"Gyro timestamp: {tsp.format(gyroTs)} ms")
+            print(f"Gyro: x: {imup.format(imuData.gyroscope.x)} y: {imup.format(imuData.gyroscope.y)} z: {imup.format(imuData.gyroscope.z)} ")
 
         if cv2.waitKey(1) == ord('q'):
             break
