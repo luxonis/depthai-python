e8c42f988cdda9b9aa13e954d67cab39888d2d44
alex-luxonis <alex@luxonis.com>
2021-04-30 09:21:44 +0300

    Update core/FW: initial ColorCamera support for OV9782

--- tests/cam_test.py
+++ tests/cam_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-os.environ["DEPTHAI_LEVEL"] = "debug"
+#os.environ["DEPTHAI_LEVEL"] = "debug"
 
 import cv2
 import argparse
@@ -12,7 +12,13 @@ parser.add_argument('-res', '--resolution', type=int, default=400, choices={400,
                     help="Select camera resolution (height). Default: %(default)s")
 parser.add_argument('-cam', '--camera-socket', default='left', choices={'left', 'right', 'rgb'},
                     help="Select camera socket")
+parser.add_argument('-color', '--color-cam', default=False, action="store_true",
+                    help="Create ColorCamera instead of MonoCamera")
 args = parser.parse_args()
+
+if args.color_cam and args.resolution == 400:
+    print('OV9782 ColorCamera 400p resolution not supported. Defaulting to 800p')
+    args.resolution = 800
 
 print("DepthAI version:", dai.__version__)
 print("DepthAI path:", dai.__file__)
@@ -23,22 +29,35 @@ cam_socket_opts = {
     'right': dai.CameraBoardSocket.RIGHT,
 }
 
-cam_res_opts = {
+mono_res_opts = {
     400: dai.MonoCameraProperties.SensorResolution.THE_400_P,
     720: dai.MonoCameraProperties.SensorResolution.THE_720_P,
     800: dai.MonoCameraProperties.SensorResolution.THE_800_P,
 }
 
+color_res_opts = {
+    720: dai.ColorCameraProperties.SensorResolution.THE_720_P,
+    800: dai.ColorCameraProperties.SensorResolution.THE_800_P,
+}
+
 # Start defining a pipeline
 pipeline = dai.Pipeline()
 
-cam = pipeline.createMonoCamera()
+if args.color_cam:
+    cam = pipeline.createColorCamera()
+    cam.setResolution(color_res_opts[args.resolution])
+else:
+    cam = pipeline.createMonoCamera()
+    cam.setResolution(mono_res_opts[args.resolution])
+
 cam.setBoardSocket(cam_socket_opts[args.camera_socket])
-cam.setResolution(cam_res_opts[args.resolution])
 
 xout = pipeline.createXLinkOut()
 xout.setStreamName(args.camera_socket)
-cam.out.link(xout.input)
+if args.color_cam:
+    cam.isp.link(xout.input)
+else:
+    cam.out.link(xout.input)
 
 # Pipeline is defined, now we can connect to the device
 with dai.Device(pipeline) as device:
