8581e64b4f8960f0eee13aa23c8a275ada3301dc
SzabolcsGergely <szabi@luxonis.com>
2021-05-03 11:24:18 +0300

    Add custom string representation for tracking status to the front of overload chain

--- src/DatatypeBindings.cpp
+++ src/DatatypeBindings.cpp
@@ -277,6 +277,16 @@ void DatatypeBindings::bind(pybind11::module& m){
         .value("TRACKED", Tracklet::TrackingStatus::TRACKED)
         .value("LOST", Tracklet::TrackingStatus::LOST)
         .value("REMOVED", Tracklet::TrackingStatus::REMOVED)
+        .def("__str__", [](const Tracklet::TrackingStatus& v) {
+            std::stringstream stream;
+            stream << v;
+            return stream.str();
+        }, py::prepend{})
+        // .def("__repr__", [](const Tracklet::TrackingStatus& v) {
+        //     std::stringstream stream;
+        //     stream << v;
+        //     return stream.str();
+        // }, py::prepend{})
         ;
 
     // Bind RawTracklets
