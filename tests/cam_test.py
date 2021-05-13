6a7607375d74c6204a65ce105d3ad7a4f8eb5d6e
alex-luxonis <alex@luxonis.com>
2021-05-13 04:35:03 +0300

    cam_test.py: run all 3 cameras, by default OV9782 + 2x OV9282/9281 Note: if running with IMX378 instead, the resolution must be specified (1080/4k/12mp)

--- tests/cam_test.py
+++ tests/cam_test.py
@@ -8,17 +8,16 @@ import argparse
 import depthai as dai
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-res', '--resolution', type=int, default=400, choices={400, 720, 800},
-                    help="Select camera resolution (height). Default: %(default)s")
-parser.add_argument('-cam', '--camera-socket', default='left', choices={'left', 'right', 'rgb'},
-                    help="Select camera socket")
-parser.add_argument('-color', '--color-cam', default=False, action="store_true",
-                    help="Create ColorCamera instead of MonoCamera")
+parser.add_argument('-mres', '--mono-resolution', type=int, default=800, choices={400, 720, 800},
+                    help="Select mono camera resolution (height). Default: %(default)s")
+parser.add_argument('-cres', '--color-resolution', default='800', choices={'720', '800', '1080', '4k', '12mp'},
+                    help="Select color camera resolution / height. Default: %(default)s")
+parser.add_argument('-rot', '--rotate', const='all', choices={'all', 'rgb', 'mono'}, nargs="?",
+                    help="Which cameras to rotate 180 degrees. All if not filtered")
 args = parser.parse_args()
 
-if args.color_cam and args.resolution == 400:
-    print('OV9782 ColorCamera 400p resolution not supported. Defaulting to 800p')
-    args.resolution = 800
+# TODO as args
+cam_list = ['rgb', 'left', 'right']
 
 print("DepthAI version:", dai.__version__)
 print("DepthAI path:", dai.__file__)
@@ -29,6 +28,12 @@ cam_socket_opts = {
     'right': dai.CameraBoardSocket.RIGHT,
 }
 
+rotate = {
+    'rgb'  : args.rotate in ['all', 'rgb'],
+    'left' : args.rotate in ['all', 'mono'],
+    'right': args.rotate in ['all', 'mono'],
+}
+
 mono_res_opts = {
     400: dai.MonoCameraProperties.SensorResolution.THE_400_P,
     720: dai.MonoCameraProperties.SensorResolution.THE_720_P,
@@ -36,39 +41,44 @@ mono_res_opts = {
 }
 
 color_res_opts = {
-    720: dai.ColorCameraProperties.SensorResolution.THE_720_P,
-    800: dai.ColorCameraProperties.SensorResolution.THE_800_P,
+    '720':  dai.ColorCameraProperties.SensorResolution.THE_720_P,
+    '800':  dai.ColorCameraProperties.SensorResolution.THE_800_P,
+    '1080': dai.ColorCameraProperties.SensorResolution.THE_1080_P,
+    '4k':   dai.ColorCameraProperties.SensorResolution.THE_4_K,
+    '12mp': dai.ColorCameraProperties.SensorResolution.THE_12_MP,
 }
 
 # Start defining a pipeline
 pipeline = dai.Pipeline()
 
-if args.color_cam:
-    cam = pipeline.createColorCamera()
-    cam.setResolution(color_res_opts[args.resolution])
-else:
-    cam = pipeline.createMonoCamera()
-    cam.setResolution(mono_res_opts[args.resolution])
-
-cam.setBoardSocket(cam_socket_opts[args.camera_socket])
-
-xout = pipeline.createXLinkOut()
-xout.setStreamName(args.camera_socket)
-if args.color_cam:
-    cam.isp.link(xout.input)
-else:
-    cam.out.link(xout.input)
+cam = {}
+xout = {}
+for c in cam_list:
+    xout[c] = pipeline.createXLinkOut()
+    xout[c].setStreamName(c)
+    if c == 'rgb':
+        cam[c] = pipeline.createColorCamera()
+        cam[c].setResolution(color_res_opts[args.color_resolution])
+        cam[c].isp.link(xout[c].input)
+    else:
+        cam[c] = pipeline.createMonoCamera()
+        cam[c].setResolution(mono_res_opts[args.mono_resolution])
+        cam[c].out.link(xout[c].input)
+    cam[c].setBoardSocket(cam_socket_opts[c])
+    if rotate[c]:
+        cam[c].setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
 
 # Pipeline is defined, now we can connect to the device
 with dai.Device(pipeline) as device:
-    # Start pipeline
-    device.startPipeline()
-
-    q = device.getOutputQueue(name=args.camera_socket, maxSize=4, blocking=False)
+    q = {}
+    for c in cam_list:
+        q[c] = device.getOutputQueue(name=c, maxSize=4, blocking=False)
 
     while True:
-        frame = q.get().getCvFrame()
-        cv2.imshow(args.camera_socket, frame)
-
+        for c in cam_list:
+            pkt = q[c].tryGet()
+            if pkt is not None:
+                frame = pkt.getCvFrame()
+                cv2.imshow(c, frame)
         if cv2.waitKey(1) == ord('q'):
             break
