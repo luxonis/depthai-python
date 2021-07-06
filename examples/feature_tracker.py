2bd58afa8efac0617fa032f4e64a6b22d2d6d1eb
SzabolcsGergely <szabi@luxonis.com>
2021-07-06 20:47:57 +0300

    Do tracking on 1280x800

--- examples/feature_tracker.py
+++ examples/feature_tracker.py
@@ -3,10 +3,6 @@
 import cv2
 import depthai as dai
 
-stepSize = 0.05
-
-newConfig = False
-
 # Create pipeline
 pipeline = dai.Pipeline()
 
@@ -18,23 +14,21 @@ spatialLocationCalculator = pipeline.createSpatialLocationCalculator()
 xoutDepth = pipeline.createXLinkOut()
 
 
-xoutDepth.setStreamName("depth")
+xoutDepth.setStreamName("features")
 
 
 # Properties
-monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_800_P)
 monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_800_P)
 monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 
 
-# Config
+# Config, unused
 topLeft = dai.Point2f(0.4, 0.4)
 bottomRight = dai.Point2f(0.6, 0.6)
 
 config = dai.SpatialLocationCalculatorConfigData()
-config.depthThresholds.lowerThreshold = 100
-config.depthThresholds.upperThreshold = 10000
 config.roi = dai.Rect(topLeft, bottomRight)
 
 spatialLocationCalculator.setWaitForConfigInput(False)
@@ -50,11 +44,7 @@ monoLeft.out.link(spatialLocationCalculator.inputDepth)
 with dai.Device(pipeline) as device:
 
     # Output queue will be used to get the depth frames from the outputs defined above
-    featureQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
-
-    color = (255, 255, 255)
-
-    print("Use WASD keys to move ROI!")
+    featureQueue = device.getOutputQueue(name="features", maxSize=4, blocking=False)
 
     while True:
         inFeatures = featureQueue.get() # Blocking call, will wait until a new data has arrived
