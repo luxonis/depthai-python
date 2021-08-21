39871af4763a2434c091db3cd33c74aaef3816ae
TheMarpe <martin.peterlin7@gmail.com>
2021-08-21 22:35:47 +0200

    Updated flash_bootloader example

--- examples/flash_bootloader.py
+++ examples/flash_bootloader.py
@@ -4,7 +4,7 @@ import depthai as dai
 import sys
 import time
 
-blType = dai.DeviceBootloader.Type.USB
+blType = dai.DeviceBootloader.Type.AUTO
 if len(sys.argv) > 1:
     if sys.argv[1] == 'usb':
         blType = dai.DeviceBootloader.Type.USB
@@ -13,9 +13,6 @@ if len(sys.argv) > 1:
     else:
         print("Specify either 'usb' or 'network' bootloader type")
         exit()
-else:
-    print(f"Usage: {sys.argv[0]} <usb/network>")
-    exit()
 
 print("Warning! Flashing bootloader can potentially soft brick your device and should be done with caution.")
 print("Do not unplug your device while the bootloader is flashing.")
@@ -30,11 +27,12 @@ if not found:
     exit(-1)
 
 # Open DeviceBootloader and allow flashing bootloader
+print(f"Booting latest bootloader first, will take a tad longer...")
 with dai.DeviceBootloader(info, allowFlashingBootloader=True) as bl:
     currentBlType = bl.getType()
 
     # Check if bootloader type is the same
-    if currentBlType != blType:
+    if blType != dai.DeviceBootloader.Type.AUTO and currentBlType != blType:
         print(f"Are you sure you want to flash '{blType.name}' bootloader over current '{currentBlType.name}' bootloader?")
         print(f"Type 'y' and press enter to proceed, otherwise exits: ")
         if input() != 'y':
@@ -44,9 +42,9 @@ with dai.DeviceBootloader(info, allowFlashingBootloader=True) as bl:
     # Create a progress callback lambda
     progress = lambda p : print(f'Flashing progress: {p*100:.1f}%')
 
-    print(f"Flashing {blType.name} bootloader...")
+    print(f"Flashing {currentBlType.name} bootloader...")
     startTime = time.monotonic()
-    (res, message) = bl.flashBootloader(dai.DeviceBootloader.Memory.FLASH, blType, progress)
+    (res, message) = bl.flashBootloader(dai.DeviceBootloader.Memory.FLASH, currentBlType, progress)
     if res:
         print("Flashing successful. Took", time.monotonic() - startTime, "seconds")
     else:
