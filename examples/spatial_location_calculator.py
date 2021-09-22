cbc537e97028cd4217c9e94cc741bac259ecd601
TheMarpe <martin.peterlin7@gmail.com>
2021-09-23 02:06:25 +0300

    Removed deprecated usage

--- examples/spatial_location_calculator.py
+++ examples/spatial_location_calculator.py
@@ -46,7 +46,7 @@ config.depthThresholds.lowerThreshold = 100
 config.depthThresholds.upperThreshold = 10000
 config.roi = dai.Rect(topLeft, bottomRight)
 
-spatialLocationCalculator.setWaitForConfigInput(False)
+spatialLocationCalculator.inputConfig.setWaitForMessage(False)
 spatialLocationCalculator.initialConfig.addROI(config)
 
 # Linking
