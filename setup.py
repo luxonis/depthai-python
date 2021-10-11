e787dbd89ea6dfa2b7c2f9845d452f5404efd881
alex-luxonis <alex@luxonis.com>
2021-10-11 15:09:39 +0300

    GitHub CI: '3.10' quoted, to not end up as 3.1. setup.py: add 3.10 to package description

--- setup.py
+++ setup.py
@@ -204,6 +204,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: C++",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering",
