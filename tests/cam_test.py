c51329206e59410f2bc2d2889f13145ec7442884
alex-luxonis <alex@luxonis.com>
2021-11-11 04:03:50 +0200

    Update FW: populate `CameraProperties::hasAutofocus`

--- tests/cam_test.py
+++ tests/cam_test.py
@@ -163,7 +163,8 @@ with dai.Device(pipeline) as device:
     #print('Connected cameras:', [c.name for c in device.getConnectedCameras()])
     print('Connected cameras:')
     for p in device.getConnectedCameraProperties():
-        print(f' -socket {p.socket.name:6}: {p.sensorName:6} {p.width:4} x {p.height:4} - ', end='')
+        print(f' -socket {p.socket.name:6}: {p.sensorName:6} {p.width:4} x {p.height:4} focus:', end='')
+        print('auto ' if p.hasAutofocus else 'fixed', '- ', end='')
         print(*[type.name for type in p.supportedTypes])
 
     print('USB speed:', device.getUsbSpeed().name)
