f58d6b1876e03df2746ff4500368f18c0f3eed4b
SzabolcsGergely <szabi@luxonis.com>
2022-02-22 14:31:34 +0200

    Modify IMU example: GYRO at 400 hz to avoid spikes

--- examples/IMU/imu_gyroscope_accelerometer.py
+++ examples/IMU/imu_gyroscope_accelerometer.py
@@ -14,8 +14,11 @@ xlinkOut = pipeline.create(dai.node.XLinkOut)
 
 xlinkOut.setStreamName("imu")
 
-# enable ACCELEROMETER_RAW and GYROSCOPE_RAW at 500 hz rate
-imu.enableIMUSensor([dai.IMUSensor.ACCELEROMETER_RAW, dai.IMUSensor.GYROSCOPE_RAW], 500)
+# enable ACCELEROMETER_RAW at 500 hz rate
+imu.enableIMUSensor(dai.IMUSensor.ACCELEROMETER_RAW, 500)
+# enable GYROSCOPE_RAW at 400 hz rate
+imu.enableIMUSensor(dai.IMUSensor.GYROSCOPE_RAW, 400)
+# it's recommended to set both setBatchReportThreshold and setMaxBatchReports to 20 when integrating in a pipeline with a lot of input/output connections
 # above this threshold packets will be sent in batch of X, if the host is not blocked and USB bandwidth is available
 imu.setBatchReportThreshold(1)
 # maximum number of IMU packets in a batch, if it's reached device will block sending until host can receive it
