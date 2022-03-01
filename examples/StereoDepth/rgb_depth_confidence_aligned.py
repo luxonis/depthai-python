26c48c21765b5bbeabdfe5219f7a0cff7d0481b4
OanaMariaVatavu <oana@luxonis.com>
2022-03-01 20:54:09 +0200

    Create a new StereoDepth python example based on 7270e96c8100cdce7000c19f6368b655aa8d53ab commit

---
+++ examples/StereoDepth/rgb_depth_confidence_aligned.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+import depthai as dai
+
+# Weights to use when blending depth/rgb image (should equal 1.0)
+rgbWeight = 0.4
+depthWeight = 0.6
+
+
+def updateBlendWeights(percent_rgb):
+    """
+    Update the rgb and depth weights used to blend depth/rgb image
+
+    @param[in] percent_rgb The rgb weight expressed as a percentage (0..100)
+    """
+    global depthWeight
+    global rgbWeight
+    rgbWeight = float(percent_rgb)/100.0
+    depthWeight = 1.0 - rgbWeight
+
+
+# Optional. If set (True), the ColorCamera is downscaled from 1080p to 720p.
+# Otherwise (False), the aligned depth is automatically upscaled to 1080p
+downscaleColor = True
+fps = 30
+# The disparity is computed at this resolution, then upscaled to RGB resolution
+monoResolution = dai.MonoCameraProperties.SensorResolution.THE_720_P
+
+# Create pipeline
+pipeline = dai.Pipeline()
+queueNames = []
+
+# Define sources and outputs
+camRgb = pipeline.create(dai.node.ColorCamera)
+left = pipeline.create(dai.node.MonoCamera)
+right = pipeline.create(dai.node.MonoCamera)
+stereo = pipeline.create(dai.node.StereoDepth)
+
+rgbOut = pipeline.create(dai.node.XLinkOut)
+disparityOut = pipeline.create(dai.node.XLinkOut)
+
+rgbOut.setStreamName("rgb")
+queueNames.append("rgb")
+disparityOut.setStreamName("disp")
+queueNames.append("disp")
+
+#Properties
+camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setFps(fps)
+if downscaleColor: camRgb.setIspScale(2, 3)
+# For now, RGB needs fixed focus to properly align with depth.
+# This value was used during calibration
+camRgb.initialControl.setManualFocus(130)
+
+left.setResolution(monoResolution)
+left.setBoardSocket(dai.CameraBoardSocket.LEFT)
+left.setFps(fps)
+right.setResolution(monoResolution)
+right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+right.setFps(fps)
+
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+# LR-check is required for depth alignment
+stereo.setLeftRightCheck(True)
+if 0: stereo.setSubpixel(True)  # TODO enable for test
+stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
+
+xoutConfMap = pipeline.create(dai.node.XLinkOut)
+xoutConfMap.setStreamName('confidence_map')
+
+# Linking
+camRgb.isp.link(rgbOut.input)
+left.out.link(stereo.left)
+right.out.link(stereo.right)
+stereo.disparity.link(disparityOut.input)
+stereo.confidenceMap.link(xoutConfMap.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+
+    frameRgb = None
+    frameDisp = None
+
+    # Configure windows; trackbar adjusts blending ratio of rgb/depth
+    rgbWindowName = "rgb"
+    depthWindowName = "depth"
+    blendedWindowName = "rgb-depth"
+    cv2.namedWindow(rgbWindowName)
+    cv2.namedWindow(depthWindowName)
+    cv2.namedWindow(blendedWindowName)
+    cv2.createTrackbar('RGB Weight %', blendedWindowName, int(rgbWeight*100), 100, updateBlendWeights)
+
+    while True:
+        latestPacket = {}
+        latestPacket["rgb"] = None
+        latestPacket["disp"] = None
+        latestPacket["confidence_map"] = None
+
+        queueEvents = device.getQueueEvents(("rgb", "disp", "confidence_map"))
+        for queueName in queueEvents:
+            packets = device.getOutputQueue(queueName).tryGetAll()
+            if len(packets) > 0:
+                latestPacket[queueName] = packets[-1]
+
+        if latestPacket["rgb"] is not None:
+            frameRgb = latestPacket["rgb"].getCvFrame()
+            cv2.imshow(rgbWindowName, frameRgb)
+
+        if latestPacket["confidence_map"] is not None:
+            frameC = latestPacket["confidence_map"].getCvFrame()
+            cv2.imshow("conf", frameC)
+
+        if latestPacket["disp"] is not None:
+            frameDisp = latestPacket["disp"].getFrame()
+            maxDisparity = stereo.initialConfig.getMaxDisparity()
+            # Optional, extend range 0..95 -> 0..255, for a better visualisation
+            if 1: frameDisp = (frameDisp * 255. / maxDisparity).astype(np.uint8)
+            # Optional, apply false colorization
+            if 1: frameDisp = cv2.applyColorMap(frameDisp, cv2.COLORMAP_HOT)
+            frameDisp = np.ascontiguousarray(frameDisp)
+            cv2.imshow(depthWindowName, frameDisp)
+
+        # Blend when both received
+        if frameRgb is not None and frameDisp is not None:
+            # Need to have both frames in BGR format before blending
+            if len(frameDisp.shape) < 3:
+                frameDisp = cv2.cvtColor(frameDisp, cv2.COLOR_GRAY2BGR)
+            blended = cv2.addWeighted(frameRgb, rgbWeight, frameDisp, depthWeight, 0)
+            cv2.imshow(blendedWindowName, blended)
+            frameRgb = None
+            frameDisp = None
+
+        if cv2.waitKey(1) == ord('q'):
+            break
