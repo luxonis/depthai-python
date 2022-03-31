254c3dfb08d15197addcd2800b1cf42d05dd58e8
zigakerzan <ziga.kerzan@gmail.com>
2022-03-31 19:18:51 +0200

    Changed calibration reader
    Reader now prints only RGB info if the camera name is OAK-1

--- examples/calibration/calibration_reader.py
+++ examples/calibration/calibration_reader.py
@@ -20,41 +20,43 @@ with dai.Device() as device:
     print(width)
     print(height)
 
-    M_left = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.LEFT, 1280, 720))
-    print("LEFT Camera resized intrinsics...")
-    print(M_left)
+    if "OAK-1" in calibData.getEepromData().boardName:
+        print(f'RGB FOV {calibData.getFov(dai.CameraBoardSocket.RGB)}')
+    else:
+        M_left = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.LEFT, 1280, 720))
+        print("LEFT Camera resized intrinsics...")
+        print(M_left)
 
-    D_left = np.array(calibData.getDistortionCoefficients(dai.CameraBoardSocket.LEFT))
-    print("LEFT Distortion Coefficients...")
-    [print(name+": "+value) for (name, value) in zip(["k1","k2","p1","p2","k3","k4","k5","k6","s1","s2","s3","s4","τx","τy"],[str(data) for data in D_left])]
+        D_left = np.array(calibData.getDistortionCoefficients(dai.CameraBoardSocket.LEFT))
+        print("LEFT Distortion Coefficients...")
+        [print(name+": "+value) for (name, value) in zip(["k1","k2","p1","p2","k3","k4","k5","k6","s1","s2","s3","s4","τx","τy"],[str(data) for data in D_left])]
 
-    M_right = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.RIGHT, 1280, 720))
-    print("RIGHT Camera resized intrinsics...")
-    print(M_right)
+        M_right = np.array(calibData.getCameraIntrinsics(dai.CameraBoardSocket.RIGHT, 1280, 720))
+        print("RIGHT Camera resized intrinsics...")
+        print(M_right)
 
-    D_right = np.array(calibData.getDistortionCoefficients(dai.CameraBoardSocket.RIGHT))
-    print("RIGHT Distortion Coefficients...")
-    [print(name+": "+value) for (name, value) in zip(["k1","k2","p1","p2","k3","k4","k5","k6","s1","s2","s3","s4","τx","τy"],[str(data) for data in D_right])]
+        D_right = np.array(calibData.getDistortionCoefficients(dai.CameraBoardSocket.RIGHT))
+        print("RIGHT Distortion Coefficients...")
+        [print(name+": "+value) for (name, value) in zip(["k1","k2","p1","p2","k3","k4","k5","k6","s1","s2","s3","s4","τx","τy"],[str(data) for data in D_right])]
 
-    print(f"RGB FOV {calibData.getFov(dai.CameraBoardSocket.RGB)}, Mono FOV {calibData.getFov(dai.CameraBoardSocket.LEFT)}")
+        print(f"RGB FOV {calibData.getFov(dai.CameraBoardSocket.RGB)}, Mono FOV {calibData.getFov(dai.CameraBoardSocket.LEFT)}")
 
-    R1 = np.array(calibData.getStereoLeftRectificationRotation())
-    R2 = np.array(calibData.getStereoRightRectificationRotation())
-    M_right = np.array(calibData.getCameraIntrinsics(calibData.getStereoRightCameraId(), 1280, 720))
+        R1 = np.array(calibData.getStereoLeftRectificationRotation())
+        R2 = np.array(calibData.getStereoRightRectificationRotation())
+        M_right = np.array(calibData.getCameraIntrinsics(calibData.getStereoRightCameraId(), 1280, 720))
 
-    H_left = np.matmul(np.matmul(M_right, R1), np.linalg.inv(M_left))
-    print("LEFT Camera stereo rectification matrix...")
-    print(H_left)
+        H_left = np.matmul(np.matmul(M_right, R1), np.linalg.inv(M_left))
+        print("LEFT Camera stereo rectification matrix...")
+        print(H_left)
 
-    H_right = np.matmul(np.matmul(M_right, R1), np.linalg.inv(M_right))
-    print("RIGHT Camera stereo rectification matrix...")
-    print(H_right)
+        H_right = np.matmul(np.matmul(M_right, R1), np.linalg.inv(M_right))
+        print("RIGHT Camera stereo rectification matrix...")
+        print(H_right)
 
-    lr_extrinsics = np.array(calibData.getCameraExtrinsics(dai.CameraBoardSocket.LEFT, dai.CameraBoardSocket.RIGHT))
-    print("Transformation matrix of where left Camera is W.R.T right Camera's optical center")
-    print(lr_extrinsics)
+        lr_extrinsics = np.array(calibData.getCameraExtrinsics(dai.CameraBoardSocket.LEFT, dai.CameraBoardSocket.RIGHT))
+        print("Transformation matrix of where left Camera is W.R.T right Camera's optical center")
+        print(lr_extrinsics)
 
-    l_rgb_extrinsics = np.array(calibData.getCameraExtrinsics(dai.CameraBoardSocket.LEFT, dai.CameraBoardSocket.RGB))
-    print("Transformation matrix of where left Camera is W.R.T RGB Camera's optical center")
-    print(l_rgb_extrinsics)
-
+        l_rgb_extrinsics = np.array(calibData.getCameraExtrinsics(dai.CameraBoardSocket.LEFT, dai.CameraBoardSocket.RGB))
+        print("Transformation matrix of where left Camera is W.R.T RGB Camera's optical center")
+        print(l_rgb_extrinsics)
