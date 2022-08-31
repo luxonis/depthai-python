5fda1f74b5d537e02679dab6306997b1826d334d
Erol444 <erol123444@gmail.com>
2022-08-31 18:50:10 +0200

    Fixed example code. TODO: docs

--- examples/Script/script_change_pipeline_flow.py
+++ examples/Script/script_change_pipeline_flow.py
@@ -11,6 +11,7 @@ pipeline = dai.Pipeline()
 
 cam = pipeline.createColorCamera()
 cam.setBoardSocket(dai.CameraBoardSocket.RGB)
+cam.setInterleaved(False)
 cam.setIspScale(2,3)
 cam.setVideoSize(720,720)
 cam.setPreviewSize(300,300)
@@ -27,11 +28,11 @@ xin.out.link(script.inputs['toggle'])
 
 cam.preview.link(script.inputs['rgb'])
 script.setScript("""
-toggle = True
+toggle = False
 while True:
     msg = node.io['toggle'].tryGet()
     if msg is not None:
-        toggle = not toggle
+        toggle = msg.getData()[0]
         node.warn('Toggle! Perform NN inferencing: ' + str(toggle))
 
     frame = node.io['rgb'].get()
@@ -54,20 +55,20 @@ with dai.Device(pipeline) as device:
     qRgb = device.getOutputQueue("rgb")
     qNn = device.getOutputQueue("nn")
 
+    runNn = False
+
     def frameNorm(frame, bbox):
         normVals = np.full(len(bbox), frame.shape[0])
         normVals[::2] = frame.shape[1]
         return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
 
+    color = (255, 127, 0)
     def drawDetections(frame, detections):
-        color = (255, 0, 0)
         for detection in detections:
             bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
-            cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+            cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
             cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
 
-    def send_trigger():
-        inQ.send(dai.Buffer())
 
     while True:
         frame = qRgb.get().getCvFrame()
@@ -76,11 +77,15 @@ with dai.Device(pipeline) as device:
             detections = qNn.get().detections
             drawDetections(frame, detections)
 
+        cv2.putText(frame, f"NN inferencing: {runNn}", (20,20), cv2.FONT_HERSHEY_TRIPLEX, 0.7, color)
         cv2.imshow('Color frame', frame)
 
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
         elif key == ord('t'):
-            print('send trigger to IR filter')
-            send_trigger()
+            runNn = not runNn
+            print(f"{'Enabling' if runNn else 'Disabling'} NN inferencing")
+            buf = dai.Buffer()
+            buf.setData(runNn)
+            inQ.send(buf)
