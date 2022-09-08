b29822e30d782deb9ae8100817b34aea67fb1257
alex-luxonis <alex@luxonis.com>
2022-09-08 21:33:09 +0300

    'Pipeline: add `setImageManipCmxSizeAdjust`. Update FW with 48MP fixes

--- src/pipeline/PipelineBindings.cpp
+++ src/pipeline/PipelineBindings.cpp
@@ -98,6 +98,7 @@ void PipelineBindings::bind(pybind11::module& m, void* pCallstack){
         .def("getRequiredOpenVINOVersion", &Pipeline::getRequiredOpenVINOVersion, DOC(dai, Pipeline, getRequiredOpenVINOVersion))
         .def("setCameraTuningBlobPath", &Pipeline::setCameraTuningBlobPath, py::arg("path"), DOC(dai, Pipeline, setCameraTuningBlobPath))
         .def("setXLinkChunkSize", &Pipeline::setXLinkChunkSize, py::arg("sizeBytes"), DOC(dai, Pipeline, setXLinkChunkSize))
+        .def("setImageManipCmxSizeAdjust", &Pipeline::setImageManipCmxSizeAdjust, py::arg("sizeAdjustBytes"), DOC(dai, Pipeline, setImageManipCmxSizeAdjust))
         .def("setCalibrationData", &Pipeline::setCalibrationData, py::arg("calibrationDataHandler"), DOC(dai, Pipeline, setCalibrationData))
         .def("getCalibrationData", &Pipeline::getCalibrationData, DOC(dai, Pipeline, getCalibrationData))
         .def("getDeviceConfig", &Pipeline::getDeviceConfig, DOC(dai, Pipeline, getDeviceConfig))
