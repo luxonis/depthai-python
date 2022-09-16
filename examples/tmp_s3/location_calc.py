40d4316e552cb197b82cb13b67b78a90ccfb00d6
Matevz Morato <matevz.morato@gmail.com>
2022-09-16 15:06:24 +0200

    Add location calculator

--- examples/tmp_s3/location_calc.py
+++ examples/tmp_s3/location_calc.py
@@ -33,11 +33,18 @@ xoutRight = pipeline.create(dai.node.XLinkOut)
 xoutDepth = pipeline.create(dai.node.XLinkOut)
 stereo = pipeline.create(dai.node.StereoDepth)
 
+spatialLocationCalculator = pipeline.create(dai.node.SpatialLocationCalculator)
+xoutSpatialData = pipeline.create(dai.node.XLinkOut)
+xinSpatialCalcConfig = pipeline.create(dai.node.XLinkIn)
+
 monoLeft.setStreamName("inLeft")
 monoRight.setStreamName("inRight")
 xoutLeft.setStreamName("left")
 xoutRight.setStreamName("right")
 xoutDepth.setStreamName("depth")
+
+xoutSpatialData.setStreamName("spatialData")
+xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
 
 monoLeft.setMaxDataSize(1280 * 720)
 monoRight.setMaxDataSize(1280 * 720)
@@ -48,6 +55,23 @@ monoRight.out.link(stereo.right)
 stereo.syncedLeft.link(xoutLeft.input)
 stereo.syncedRight.link(xoutRight.input)
 stereo.depth.link(xoutDepth.input)
+
+stereo.depth.link(spatialLocationCalculator.inputDepth)
+spatialLocationCalculator.out.link(xoutSpatialData.input)
+xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
+
+# Config
+topLeft = dai.Point2f(0.3, 0.4)
+bottomRight = dai.Point2f(0.6, 0.7)
+
+config = dai.SpatialLocationCalculatorConfigData()
+config.depthThresholds.lowerThreshold = 100
+config.depthThresholds.upperThreshold = 10000
+config.roi = dai.Rect(topLeft, bottomRight)
+
+spatialLocationCalculator.inputConfig.setWaitForMessage(False)
+spatialLocationCalculator.initialConfig.addROI(config)
+
 stereo.setInputResolution(1280, 720)
 
 
@@ -62,11 +86,14 @@ with dai.Device(pipeline) as device:
     qLeft = device.getOutputQueue("left", 4, False)
     qRight = device.getOutputQueue("right", 4, False)
     qDepth = device.getOutputQueue("depth", 4, False)
+    spatialCalcQueue = device.getOutputQueue(name="spatialData", maxSize=4, blocking=False)
+    spatialCalcConfigInQueue = device.getInputQueue("spatialCalcConfig")
 
     cnt = 0
 
     width = 1280
     height = 720
+    color = (255, 255, 255)
 
     while True:
 
@@ -90,6 +117,9 @@ with dai.Device(pipeline) as device:
         img.setHeight(height)
         qInRight.send(img)
 
+
+
+        spatialData = spatialCalcQueue.get().getSpatialLocations()
         inLeft = qLeft.get()
         frameLeft = inLeft.getCvFrame()
         cv2.imshow("left", frameLeft)
@@ -99,11 +129,30 @@ with dai.Device(pipeline) as device:
         cv2.imshow("right", frameRight)
 
         inDepth = qDepth.get()
-        frameDepth = inDepth.getCvFrame()
+        depthFrame = inDepth.getCvFrame()
         # cv2.imshow("depth", frameDepth)
+        depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
+        depthFrameColor = cv2.equalizeHist(depthFrameColor)
+        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+        for depthData in spatialData:
+            roi = depthData.config.roi
+            roi = roi.denormalize(width=depthFrameColor.shape[1], height=depthFrameColor.shape[0])
+            xmin = int(roi.topLeft().x)
+            ymin = int(roi.topLeft().y)
+            xmax = int(roi.bottomRight().x)
+            ymax = int(roi.bottomRight().y)
 
-        disp = (frameDepth / 32).astype(np.uint8)
-        cv2.imshow("disp", disp)
+            depthMin = depthData.depthMin
+            depthMax = depthData.depthMax
+
+            fontType = cv2.FONT_HERSHEY_TRIPLEX
+            cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+            cv2.putText(depthFrameColor, f"X: {int(depthData.spatialCoordinates.x)} mm", (xmin + 10, ymin + 20), fontType, 0.5, 255)
+            cv2.putText(depthFrameColor, f"Y: {int(depthData.spatialCoordinates.y)} mm", (xmin + 10, ymin + 35), fontType, 0.5, 255)
+            cv2.putText(depthFrameColor, f"Z: {int(depthData.spatialCoordinates.z)} mm", (xmin + 10, ymin + 50), fontType, 0.5, 255)
+        # Show the frame
+
+        cv2.imshow("disp", depthFrameColor)
 
         cnt += 1
 
