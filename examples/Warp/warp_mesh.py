56de7f7bb25d77712f611afe7307f73d7c467b23
TheMarpe <martin.peterlin7@gmail.com>
2022-09-22 13:57:39 +0200

    Added Warp node

--- examples/Warp/warp_mesh.py
+++ examples/Warp/warp_mesh.py
@@ -7,12 +7,12 @@ import numpy as np
 pipeline = dai.Pipeline()
 
 camRgb = pipeline.create(dai.node.ColorCamera)
-camRgb.setPreviewSize(500, 500)
+camRgb.setPreviewSize(496, 496)
 camRgb.setInterleaved(False)
 maxFrameSize = camRgb.getPreviewWidth() * camRgb.getPreviewHeight() * 3
 
 # Warp preview frame 1
-manip1 = pipeline.create(dai.node.Warp)
+warp1 = pipeline.create(dai.node.Warp)
 # Create a custom warp mesh
 tl = dai.Point2f(20, 20)
 tr = dai.Point2f(460, 20)
@@ -20,29 +20,31 @@ ml = dai.Point2f(100, 250)
 mr = dai.Point2f(400, 250)
 bl = dai.Point2f(20, 460)
 br = dai.Point2f(460, 460)
-manip1.setWarpMesh([tl,tr,ml,mr,bl,br], 2, 3)
-manip1.setMaxOutputFrameSize(maxFrameSize)
+warp1.setWarpMesh([tl,tr,ml,mr,bl,br], 2, 3)
+WARP1_OUTPUT_FRAME_SIZE = (992,500)
+warp1.setOutputSize(WARP1_OUTPUT_FRAME_SIZE)
+warp1.setMaxOutputFrameSize(WARP1_OUTPUT_FRAME_SIZE[0] * WARP1_OUTPUT_FRAME_SIZE[1] * 3)
 
-camRgb.preview.link(manip1.inputImage)
+camRgb.preview.link(warp1.inputImage)
 xout1 = pipeline.create(dai.node.XLinkOut)
 xout1.setStreamName('out1')
-manip1.out.link(xout1.input)
+warp1.out.link(xout1.input)
 
 # Warp preview frame 2
-manip2 = pipeline.create(dai.node.ImageManip);
+warp2 = pipeline.create(dai.node.Warp);
 # Create a custom warp mesh
 mesh2 = [
     (20, 20), (250, 100), (460, 20),
     (100, 250), (250, 250), (400, 250),
     (20, 480), (250, 400), (460,480)
 ]
-manip2.setWarpMesh(mesh2, 3, 3);
-manip2.setMaxOutputFrameSize(maxFrameSize);
+warp2.setWarpMesh(mesh2, 3, 3);
+warp2.setMaxOutputFrameSize(maxFrameSize);
 
-camRgb.preview.link(manip2.inputImage);
+camRgb.preview.link(warp2.inputImage);
 xout2 = pipeline.create(dai.node.XLinkOut)
 xout2.setStreamName('out2')
-manip2.out.link(xout2.input)
+warp2.out.link(xout2.input)
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
