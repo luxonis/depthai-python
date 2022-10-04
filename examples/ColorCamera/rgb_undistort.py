38f805a0bdebffde2b947e83aaaa347eaeb35dd2
Erol444 <erol123444@gmail.com>
2022-10-04 19:28:31 +0200

    addressed PR reviews

--- examples/ColorCamera/rgb_undistort.py
+++ examples/ColorCamera/rgb_undistort.py
@@ -1,22 +1,16 @@
-import time
 import cv2
 import depthai as dai
 import numpy as np
 
-resolutionMap = {"THE_1080_P": (1920, 1080), "THE_800_P": (1280, 800), "THE_720_P": (1280, 720), "THE_400_P": (640, 400)}
-resolution = resolutionMap["THE_1080_P"]
+camRes = dai.ColorCameraProperties.SensorResolution.THE_1080_P
 camSocket = dai.CameraBoardSocket.RGB
-meshWidth = 2
-meshHeight = 2
+ispScale = (1,2)
 
-def getMesh(calibData):
-    global meshWidth
-    global meshHeight
-
-    M1 = np.array(calibData.getCameraIntrinsics(camSocket, resolution[0], resolution[1]))
+def getMesh(calibData, ispSize):
+    M1 = np.array(calibData.getCameraIntrinsics(camSocket, ispSize[0], ispSize[1]))
     d1 = np.array(calibData.getDistortionCoefficients(camSocket))
     R1 = np.identity(3)
-    mapX, mapY = cv2.initUndistortRectifyMap(M1, d1, R1, M1, resolution, cv2.CV_32FC1)
+    mapX, mapY = cv2.initUndistortRectifyMap(M1, d1, R1, M1, ispSize, cv2.CV_32FC1)
 
     meshCellSize = 16
     mesh0 = []
@@ -51,59 +45,45 @@ def getMesh(calibData):
 
     mesh = list(map(tuple, mesh0))
 
-    return mesh
+    return mesh, meshWidth, meshHeight
 
-def create_pipeline(mesh):
+def create_pipeline(calibData):
     pipeline = dai.Pipeline()
 
     cam = pipeline.create(dai.node.ColorCamera)
+    cam.setIspScale(ispScale)
     cam.setBoardSocket(camSocket)
-    cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    cam.setIspScale(1, 1)
+    cam.setResolution(camRes)
 
-    manip1 = pipeline.create(dai.node.ImageManip)
-    manip1.setWarpMesh(mesh, meshWidth, meshHeight)
-    manip1.setMaxOutputFrameSize(cam.getIspWidth() * cam.getIspHeight() * 3 // 2)
+    manip = pipeline.create(dai.node.ImageManip)
+    mesh, meshWidth, meshHeight = getMesh(calibData, cam.getIspSize())
+    manip.setWarpMesh(mesh, meshWidth, meshHeight)
+    manip.setMaxOutputFrameSize(cam.getIspWidth() * cam.getIspHeight() * 3 // 2)
     # TODO manip1.initialConfig.setWarpBorderFillColor(0, 0, 0)
+    cam.isp.link(manip.inputImage)
 
-    cam.isp.link(manip1.inputImage)
     cam_xout = pipeline.createXLinkOut()
-    cam_xout.setStreamName("cam_out")
+    cam_xout.setStreamName("Undistorted")
+    manip.out.link(cam_xout.input)
 
-    if 1: # direct
-        manip1.out.link(cam_xout.input)
-    else: # downscaled
-        manip2 = pipeline.create(dai.node.ImageManip)
-        manip2.initialConfig.setResize(640, 400)
-        manip2.initialConfig.setKeepAspectRatio(False)
-
-        manip1.out.link(manip2.inputImage)
-        manip2.out.link(cam_xout.input)
+    dist_xout = pipeline.createXLinkOut()
+    dist_xout.setStreamName("Distorted")
+    cam.isp.link(dist_xout.input)
 
     return pipeline
 
 with dai.Device() as device:
 
     calibData = device.readCalibration()
-    mesh = getMesh(calibData)
-    pipeline = create_pipeline(mesh)
+    pipeline = create_pipeline(calibData)
     device.startPipeline(pipeline)
 
-    cam_out = device.getOutputQueue("cam_out", 4, False)
+    queues = [device.getOutputQueue(name, 4, False) for name in ['Undistorted', 'Distorted']]
 
-    time.sleep(2)
-    try:
-        while True:
-            frame = cam_out.get().getCvFrame()
-            if frame is not None:
-                print('getting Image')
-                cv2.imshow("Camera_view", frame)
-                if cv2.waitKey(1) == ord('q'):
-                    break
-            else:
-                print("frame not found")
+    while True:
+        for q in queues:
+            frame = q.get().getCvFrame()
+            cv2.imshow(q.getName(), frame)
 
-    except:
-        pass
-
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) == ord('q'):
+            break
