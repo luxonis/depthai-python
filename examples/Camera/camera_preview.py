bae849d70627b3df62d8822b644acc36b86652d8
Martin Peterlin <martin.peterlin7@gmail.com>
2022-12-17 04:15:16 +0100

    Updated camera_preview example

--- examples/Camera/camera_preview.py
+++ examples/Camera/camera_preview.py
@@ -1,43 +1,32 @@
 #!/usr/bin/env python3
-
 import cv2
 import depthai as dai
 
-# Connect to device and start pipeline
+# Connect to device
 with dai.Device() as device:
-    print('Connected cameras:', device.getConnectedCameraFeatures())
-    # Print out usb speed
-    print('Usb speed:', device.getUsbSpeed().name)
-
     # Create pipeline
     pipeline = dai.Pipeline()
     cams = device.getConnectedCameraFeatures()
     streams = []
     for cam in cams:
+        print(str(cam), str(cam.socket), cam.socket)
         c = pipeline.create(dai.node.Camera)
         x = pipeline.create(dai.node.XLinkOut)
-        c.preview.link(x.input)
+        c.isp.link(x.input)
+        c.setBoardSocket(cam.socket)
         stream = str(cam.socket)
+        if cam.name:
+            stream = f'{cam.name} ({stream})'
         x.setStreamName(stream)
         streams.append(stream)
-
-    # Bootloader version
-    if device.getBootloaderVersion() is not None:
-        print('Bootloader version:', device.getBootloaderVersion())
-    # Device name
-    print('Device name:', device.getDeviceName())
-
     # Start pipeline
     device.startPipeline(pipeline)
-    # Clear queue events
-    device.getQueueEvents()
-    while True:
-
-        queueName = device.getQueueEvent(streams)
-        message = device.getOutputQueue(queueName).get()
-        # Display arrived frames
-        if type(message) == dai.ImgFrame:
-            cv2.imshow(queueName, message.getCvFrame())
-
+    while not device.isClosed():
+        queueNames = device.getQueueEvents(streams)
+        for q in queueNames:
+            message = device.getOutputQueue(q).get()
+            # Display arrived frames
+            if type(message) == dai.ImgFrame:
+                cv2.imshow(q, message.getCvFrame())
         if cv2.waitKey(1) == ord('q'):
             break
