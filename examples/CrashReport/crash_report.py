e570d5876ef79c69959bf36248749db62dcf5799
SzabolcsGergely <szabi@luxonis.com>
2023-02-13 16:24:03 +0200

    Update crash report example

--- examples/CrashReport/crash_report.py
+++ examples/CrashReport/crash_report.py
@@ -7,12 +7,15 @@ from json import dump
 # Connect to device and start pipeline
 with dai.Device() as device:
 
-    crashDump = device.getCrashDump()
-    if len(crashDump.crashReports) == 0:
-        print("There was no crash dump found on your device!")
-    else:
+    if device.hasCrashDump():
+        crashDump = device.getCrashDump()
+
         json = crashDump.serializeToJson()
         print(json)
+        
         destPath = "crashDump.json"
         with open(destPath, 'w', encoding='utf-8') as f:
             dump(json, f, ensure_ascii=False, indent=4)
+    else:
+        print("There was no crash dump found on your device!")
+
