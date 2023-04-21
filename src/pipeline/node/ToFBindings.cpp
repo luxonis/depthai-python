d11f7e33bb87f0929c64ff1593dbbcb6ac78f2b3
Florin Buica <florin.buica@luxonis.com>
2023-04-21 14:51:10 +0300

    adding bindings for samsung33d

--- src/pipeline/node/ToFBindings.cpp
+++ src/pipeline/node/ToFBindings.cpp
@@ -29,15 +29,17 @@ void bind_tof(pybind11::module& m, void* pCallstack){
 
     // Properties
     tofProperties
-        .def_readwrite("dummy", &ToFProperties::dummy)
+        .def_readwrite("initialConfig", &ToFProperties::initialConfig)
         ;
 
     // Node
     tof
         .def_readonly("input", &ToF::input, DOC(dai, node, ToF, input), DOC(dai, node, ToF, input))
+        .def_readonly("inputConfig", &ToF::inputConfig, DOC(dai, node, ToF, inputConfig), DOC(dai, node, ToF, inputConfig))
         .def_readonly("depth", &ToF::depth, DOC(dai, node, ToF, depth), DOC(dai, node, ToF, depth))
         .def_readonly("amplitude", &ToF::amplitude, DOC(dai, node, ToF, amplitude), DOC(dai, node, ToF, amplitude))
         .def_readonly("error", &ToF::error, DOC(dai, node, ToF, error), DOC(dai, node, ToF, error))
+        .def_readonly("initialConfig", &ToF::initialConfig, DOC(dai, node, ToF, initialConfig), DOC(dai, node, ToF, initialConfig))
     ;
     // ALIAS
     daiNodeModule.attr("ToF").attr("Properties") = tofProperties;
