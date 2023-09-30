9d3ccd935fbe3ccdc4459ec6549a38064e4163a1
Martin Peterlin <martin.peterlin7@gmail.com>
2023-09-30 23:58:29 +0200

    Fixed verticalCameraSocket bindings

--- src/pipeline/CommonBindings.cpp
+++ src/pipeline/CommonBindings.cpp
@@ -262,7 +262,6 @@ void CommonBindings::bind(pybind11::module& m, void* pCallstack){
         .def_readwrite("rectifiedRotationRight", &StereoRectification::rectifiedRotationRight)
         .def_readwrite("leftCameraSocket", &StereoRectification::leftCameraSocket)
         .def_readwrite("rightCameraSocket", &StereoRectification::rightCameraSocket)
-        .def_readwrite("verticalCameraSocket", &StereoRectification::verticalCameraSocket)
         ;
 
     // Extrinsics
@@ -307,6 +306,7 @@ void CommonBindings::bind(pybind11::module& m, void* pCallstack){
         .def_readwrite("housingExtrinsics", &EepromData::housingExtrinsics)
         .def_readwrite("stereoUseSpecTranslation", &EepromData::stereoUseSpecTranslation)
         .def_readwrite("stereoEnableDistortionCorrection", &EepromData::stereoEnableDistortionCorrection)
+        .def_readwrite("verticalCameraSocket", &EepromData::verticalCameraSocket)
         ;
     // UsbSpeed
     usbSpeed
