20a59d6ab421d0929c74f1a51e5f7ce66a972599
asahtik <asahtik@gmail.com>
2023-10-18 11:15:48 +0200

    Implemented message groups and sync node

--- src/DatatypeBindings.cpp
+++ src/DatatypeBindings.cpp
@@ -14,6 +14,7 @@ void bind_imagemanipconfig(pybind11::module& m, void* pCallstack);
 void bind_imgdetections(pybind11::module& m, void* pCallstack);
 void bind_imgframe(pybind11::module& m, void* pCallstack);
 void bind_imudata(pybind11::module& m, void* pCallstack);
+void bind_message_group(pybind11::module& m, void* pCallstack);
 void bind_nndata(pybind11::module& m, void* pCallstack);
 void bind_spatialimgdetections(pybind11::module& m, void* pCallstack);
 void bind_spatiallocationcalculatorconfig(pybind11::module& m, void* pCallstack);
@@ -40,6 +41,7 @@ void DatatypeBindings::addToCallstack(std::deque<StackFunction>& callstack) {
     callstack.push_front(bind_imgdetections);
     callstack.push_front(bind_imgframe);
     callstack.push_front(bind_imudata);
+    callstack.push_front(bind_message_group);
     callstack.push_front(bind_nndata);
     callstack.push_front(bind_spatialimgdetections);
     callstack.push_front(bind_spatiallocationcalculatorconfig);
---
+++ src/pipeline/datatype/MessageGroupBindings.cpp
@@ -0,0 +1,58 @@
+#include "DatatypeBindings.hpp"
+#include "depthai-shared/datatype/RawMessageGroup.hpp"
+#include "pipeline/CommonBindings.hpp"
+#include <unordered_map>
+#include <memory>
+
+// depthai
+#include "depthai/pipeline/datatype/MessageGroup.hpp"
+
+//pybind
+#include <pybind11/chrono.h>
+#include <pybind11/numpy.h>
+
+// #include "spdlog/spdlog.h"
+
+void bind_message_group(pybind11::module& m, void* pCallstack){
+
+    using namespace dai;
+
+    py::class_<RawMessageGroup, RawBuffer, std::shared_ptr<RawMessageGroup>> rawMessageGroup(m, "RawMessageGroup", DOC(dai, RawMessageGroup));
+    py::class_<MessageGroup, Buffer, std::shared_ptr<MessageGroup>> messageGroup(m, "MessageGroup", DOC(dai, MessageGroup));
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    // Call the rest of the type defines, then perform the actual bindings
+    Callstack* callstack = (Callstack*) pCallstack;
+    auto cb = callstack->top();
+    callstack->pop();
+    cb(m, pCallstack);
+    // Actual bindings
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+
+    // Metadata / raw
+    rawMessageGroup
+        .def(py::init<>())
+        .def_readwrite("group", &RawMessageGroup::group)
+        .def_readwrite("success", &RawMessageGroup::success)
+        ;
+
+    // Message
+    messageGroup
+        .def(py::init<>())
+        .def("__getitem__", [](MessageGroup& msg, const std::string& name) {
+            return msg[name];
+        })
+        .def("syncSuccessful", &MessageGroup::syncSuccessful, DOC(dai, MessageGroup, syncSuccessful))
+        .def("getTimestamp", &MessageGroup::Buffer::getTimestamp, DOC(dai, Buffer, getTimestamp))
+        .def("getTimestampDevice", &MessageGroup::Buffer::getTimestampDevice, DOC(dai, Buffer, getTimestampDevice))
+        .def("getSequenceNum", &MessageGroup::Buffer::getSequenceNum, DOC(dai, Buffer, getSequenceNum))
+        .def("setTimestamp", &MessageGroup::setTimestamp, DOC(dai, MessageGroup, setTimestamp))
+        .def("setTimestampDevice", &MessageGroup::setTimestampDevice, DOC(dai, MessageGroup, setTimestampDevice))
+        .def("setSequenceNum", &MessageGroup::setSequenceNum, DOC(dai, MessageGroup, setSequenceNum))
+        ;
+
+}
