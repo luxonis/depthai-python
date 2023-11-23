281c22f9f6597321add30a7934732000065e1dfc
Filip Jeretina <59307111+zrezke@users.noreply.github.com>
2023-11-23 23:04:17 +0100

    IR intensity API (#920)
    * Ir brightness control based on normalized intensity, instead of current.
    
    * Added IR intensity API.
    
    * Update docstring. Point to correct device side commit.
    
    * Bump core
    
    * Use IR intensity API in cam_test.py
    
    * Deprecated ir brightness api, fixed mono_preview_alternate_pro.py example
    
    * cam test: use step 0.05 for both dot projector and flood light
    
    * Update core
    
    * synced depthai-shared with device side
    
    * resolve shared issue.
    
    * Bump core

--- utilities/cam_test.py
+++ utilities/cam_test.py
@@ -354,10 +354,10 @@ with dai.Device(*dai_device_args) as device:
     EXP_STEP = 500  # us
     ISO_STEP = 50
     LENS_STEP = 3
-    DOT_STEP = 100
-    FLOOD_STEP = 100
-    DOT_MAX = 1200
-    FLOOD_MAX = 1500
+    DOT_STEP = 0.05
+    FLOOD_STEP = 0.05
+    DOT_MAX = 1
+    FLOOD_MAX = 1
 
     # Defaults and limits for manual focus/exposure controls
     lensPos = 150
@@ -556,25 +556,25 @@ with dai.Device(*dai_device_args) as device:
             dotIntensity = dotIntensity - DOT_STEP
             if dotIntensity < 0:
                 dotIntensity = 0
-            device.setIrLaserDotProjectorBrightness(dotIntensity)
+            device.setIrLaserDotProjectorIntensity(dotIntensity)
             print(f'IR Dot intensity:', dotIntensity)
         elif key == ord('d'):
             dotIntensity = dotIntensity + DOT_STEP
             if dotIntensity > DOT_MAX:
                 dotIntensity = DOT_MAX
-            device.setIrLaserDotProjectorBrightness(dotIntensity)
+            device.setIrLaserDotProjectorIntensity(dotIntensity)
             print(f'IR Dot intensity:', dotIntensity)
         elif key == ord('w'):
             floodIntensity = floodIntensity + FLOOD_STEP
             if floodIntensity > FLOOD_MAX:
                 floodIntensity = FLOOD_MAX
-            device.setIrFloodLightBrightness(floodIntensity)
+            device.setIrFloodLightIntensity(floodIntensity)
             print(f'IR Flood intensity:', floodIntensity)
         elif key == ord('s'):
             floodIntensity = floodIntensity - FLOOD_STEP
             if floodIntensity < 0:
                 floodIntensity = 0
-            device.setIrFloodLightBrightness(floodIntensity)
+            device.setIrFloodLightIntensity(floodIntensity)
             print(f'IR Flood intensity:', floodIntensity)
         elif key >= 0 and chr(key) in '34567890[]p\\;\'':
             if key == ord('3'):
