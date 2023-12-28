c96b1f2ce26e43d19dbe79caad342fcee7a139f5
zrezke <jeretina.filip@gmail.com>
2023-12-28 11:13:48 +0100

    Fix CI

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,7 @@
+# Pybind11 2.9.2
+hunter_config(
+    pybind11
+    VERSION "2.9.2"
+    URL "https://github.com/pybind/pybind11/archive/refs/tags/v2.9.2.tar.gz"
+    SHA1 "5e05583a210282c3251281b6ee5677915f0cbf95"
+)
