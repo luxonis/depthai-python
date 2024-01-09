6d2814e8ae0bae92ce2442af1ff914cfaee63035
Darja <darja.timaseva@luxonis.com>
2024-01-09 17:36:08 +0100

    Simplify documentation and remove unnecessary comments

--- testing/face-detection/README.md
+++ testing/face-detection/README.md
@@ -15,39 +15,23 @@ This project integrates DepthAI technology to process live camera feeds and dete
 The pipeline consists of the following nodes:
 
 ### [ColorCamera Node](https://docs.luxonis.com/projects/api/en/latest/components/nodes/color_camera/?highlight=ColorCamera)
-- **Function**: Captures video frames from the onboard camera.
-- **Input**: None.
-- **Output**: `preview` stream.
-- **Details**: This node is set to output a preview stream that is used for both display purposes and as input for the object detection network.
+- **Function**: Captures video frames directly from the camera.
+- **Output**: Outputs the `preview` stream which consists of the raw video frames.
 
 ### [MobileNetDetectionNetwork Node](https://docs.luxonis.com/projects/api/en/latest/components/nodes/mobilenet_detection_network/?highlight=MobileNetDetectionNetwork)
-- **Function**: Performs object detection on the input frames.
-- **Input**: `preview` stream from the `ColorCamera` node.
-- **Output**: `nn_detections` which include bounding boxes and object classifications.
-- **Details**: Utilizes a pre-trained MobileNet SSD model optimized for the OAK device to detect objects in real-time.
-- **Model**: MobileNet SSD, specifically `face-detection-retail-0004` automatically downloaded from the [blobconverter repository](https://artifacts.luxonis.com/artifactory/blobconverter-backup/blobs/).
+- **Function**: Analyzes the incoming `preview` stream and performs face detection via a neural network.
+- **Input**: Receives `preview` frames from the `ColorCamera` node.
+- **Output**: Provides `nn_detections` containing data about detected faces, including bounding boxes and confidence scores.
+- **Model**: Uses the `face-detection-retail-0004` model from the blobconverter repository, optimized for fast and efficient face detection.
 
 ### FaceDetectionNode (Custom Node)
-- **Function**: Annotates the video frames with detection results.
-- **Input**:
-  - `image`: Frames from the `ColorCamera` node.
+- **Function**: Enhances the frames with visual annotations based on detection results.
+- **Input**: 
+  - `image`: Video frames from the `ColorCamera` node.
   - `detections`: Detection results from the `MobileNetDetectionNetwork` node.
-- **Output**: Annotated video frames displayed on the screen.
-- **Details**: This is a custom node that integrates the `FaceDetector` class to overlay bounding boxes on detected faces and displays the processed frames. The `FaceDetector` class encapsulates the logic for drawing bounding boxes and text annotations on the frame based on the detection results. The node exits when the 'q' key is pressed. This design modularizes the face detection logic, making the code more maintainable and scalable.
-
-## How It Works
-
-1. The `ColorCamera` node captures video frames from the device's camera.
-2. Captured frames are sent to the `MobileNetDetectionNetwork` node which detects objects within the frame.
-3. Detection results are then passed to the `FaceDetectionNode`.
-4. The `FaceDetectionNode` processes the frame and the detection results, drawing bounding boxes around detected faces.
-5. The annotated frames are displayed in real-time.
-6. The application runs continuously until the user exits by pressing the 'q' key.
+- **Output**: Displays the video frames with annotated bounding boxes around detected faces.
 
 ## Running the Application
-
-To start the face detection, execute the pipeline with the following command:
-
 ```bash
 python testing/face_detection/app.py
 ```
--- testing/face-detection/app.py
+++ testing/face-detection/app.py
@@ -8,28 +8,6 @@ import blobconverter
 
 
 class FaceDetector:
-    """
-    The FaceDetector class is designed for detecting and annotating faces in image frames using OpenCV.
-    It provides functionality to draw bounding boxes and labels around detected faces,
-    with customizable colors, font scales, and thicknesses for annotations.
-
-    Attributes:
-        color (Tuple[int, int, int]): The color of the bounding box.
-        text_color (Tuple[int, int, int]): The color of the label text.
-        font_scale (float): The font scale of the label text.
-        thickness (int): The thickness of the bounding box and label text.
-
-    Methods:
-        annotate_frame(frame, detections):
-            Annotates a frame with bounding boxes and labels around detected faces.
-        _convert_to_absolute_bbox(frame, bbox) -> np.ndarray:
-            Converts relative bounding box coordinates to absolute pixel values.
-        _draw_bbox(frame, bbox):
-            Draws a bounding box around a detected face.
-        _put_label(frame, bbox):
-            Draws a label above a detected face.
-    """
-
     def __init__(
         self,
         color: Tuple[int, int, int] = (255, 0, 0),
@@ -37,55 +15,31 @@ class FaceDetector:
         font_scale: float = 0.5,
         thickness: int = 2,
     ):
+        self.label = "Face"
         self.color: Tuple[int, int, int] = color
         self.text_color: Tuple[int, int, int] = text_color
         self.font_scale: float = font_scale
         self.thickness: int = thickness
 
     def annotate_frame(self, frame: np.ndarray, detections: List[Tuple[float, float, float, float]]):
-        """
-        Draws rectangles and labels around detected faces on the image.
-        :param frame: The image on which to highlight faces.
-        :param detections: A list of detected face coordinates in the format (xmin, ymin, xmax, ymax).
-        :return: None
-        """
         for detection in detections:
             bbox = self._convert_to_absolute_bbox(frame, detection)
-            self._draw_bbox(frame, bbox)
-            self._put_label(frame, bbox)
+            self._draw_bbox_around_detected_face(frame, bbox)
+            self._put_face_label(frame, bbox)
 
     @staticmethod
     def _convert_to_absolute_bbox(frame: np.ndarray, bbox: Tuple[float, float, float, float]) -> np.ndarray:
-        """
-        Converts face bounding box coordinates from relative to absolute pixel values.
-        'bbox' is a tuple representing the coordinates of the face (xmin, ymin, xmax, ymax),
-        where 'xmin' and 'ymin' are the coordinates of the top-left corner, and 'xmax' and 'ymax'
-        are the coordinates of the bottom-right corner.
-        :param frame: The image to determine the dimensions for the bounding box.
-        :param bbox: Relative coordinates of the face bounding box.
-        :return: Absolute pixel coordinates of the bounding box.
-        """
         frame_height, frame_width = frame.shape[:2]
         xmin, ymin, xmax, ymax = bbox
         return np.array([xmin * frame_width, ymin * frame_height, xmax * frame_width, ymax * frame_height], dtype=int)
 
-    def _draw_bbox(self, frame: np.ndarray, bbox: np.ndarray):
-        """
-        Draws a bounding box around a detected face.
-        :param frame: The image frame to annotate.
-        :param bbox: The bounding box coordinates to convert.
-        """
+    def _draw_bbox_around_detected_face(self, frame: np.ndarray, bbox: np.ndarray):
         cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), self.color, self.thickness)
 
-    def _put_label(self, frame: np.ndarray, bbox: np.ndarray):
-        """
-        Draws a label above a detected face.
-        :param frame: The image frame to annotate.
-        :param bbox: The bounding box coordinates to convert.
-        """
+    def _put_face_label(self, frame: np.ndarray, bbox: np.ndarray):
         cv2.putText(
             frame,
-            "Face",
+            self.label,
             (bbox[0], bbox[1] - 10),
             cv2.FONT_HERSHEY_SIMPLEX,
             self.font_scale,
@@ -96,32 +50,17 @@ class FaceDetector:
 
 class FaceDetectionNode(dai.Node):
     """
-    This class integrates with a DepthAI pipeline to process live camera feeds and detect faces.
-    Detected faces are highlighted with rectangles in the output display. The display loop
-    can be terminated by pressing the 'q' key.
+    Custom node for visualizing the results of face detection within the DepthAI pipeline.
+    This node takes in image frames and detection data from a neural network, and uses
+    the FaceDetector class to annotate the frames with bounding boxes around detected faces.
+    The annotated frames are then displayed on the screen.
 
-    Attributes:
-        KEY_QUIT (int): ASCII code for the 'quit' key.
-
-    Methods:
-        __run__(self, image, detections):
-            Annotates and displays the camera feed with face detections and labels using special FaceDetector class.
-
-    TODO: Add visualization enhancements
+    # TODO: Add visualization enhancements
     """
 
     KEY_QUIT = ord("q")
 
     def __run__(self, image: dai.message.ImgFrame, detections: dai.message.ImgDetections):
-        """
-        Processes an image frame and detections from a neural network to perform face detection.
-        Draws bounding boxes around detected faces.
-
-        Args:
-            image (dai.message.ImgFrame): The image frame to process with type np.ndarray
-            detections (dai.message.ImgDetections): The detections from the neural network
-        """
-
         if cv2.waitKey(1) == self.KEY_QUIT:
             raise KeyboardInterrupt()
 
@@ -133,11 +72,9 @@ class FaceDetectionNode(dai.Node):
         cv2.imshow("Face Detection", frame)
 
 
-# Initialize logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Create pipeline with camera and neural network
 pipeline = dai.Pipeline()
 
 with pipeline:
