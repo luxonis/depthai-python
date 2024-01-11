365204b49846da8d62608b2798a0bd5ddef8cea3
Jakub Gál <44644846+JakubGal@users.noreply.github.com>
2024-01-11 11:21:16 +0100

    Script node example

--- testing/preview_all_cameras/README.md
+++ testing/preview_all_cameras/README.md
@@ -1,10 +1,10 @@
-Preview All Cameras
+Script change pipeline flow
 ===================
 
-This example shows how to set up a pipeline that outputs a a preview for each camera currently connected (and available) to the device. The preview is displayed in a window on the host machine.
+This example shows how you can change the flow of data inside your pipeline in runtime using the Script node. In this example, we send a message from the host to choose whether we want Script node to forwards color frame to the MobileNetDetectionNetwork
 
 ## Demo
-![demo](https://docs.luxonis.com/projects/sdk/en/latest/_images/sdk_preview_all_cameras.png)
+![demo](https://user-images.githubusercontent.com/18037362/187734814-df3b46c9-5e04-4a9d-bf6f-d738b40b4421.gif)
 
 ## Setup
 First download and install depthai when not done previously
@@ -16,4 +16,4 @@ pip install .
 
 ## Pipeline
 
-![pipeline](https://docs.luxonis.com/projects/sdk/en/latest/_images/preview_all_cameras.png)
+![pipeline](https://user-images.githubusercontent.com/18037362/187736249-db7ff175-fcea-4d4e-b567-f99087bd82ee.png)
---
+++ testing/script_node/script_node.py
@@ -0,0 +1,61 @@
+import cv2
+import blobconverter
+import numpy as np
+import depthai as dai
+
+class ControlKey:
+    QUIT = ord("q")
+    DRAW_DETECTIONS = ord("t")
+
+class PipelineFlowChangerNode(dai.Node):
+    draw_color = (255, 127, 0)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.toggle = False
+
+    def __run__(self, image: dai.message.ImgFrame, detections: dai.message.ImgDetections) -> None:
+        key = cv2.waitKey(1)
+        if key == ControlKey.QUIT:raise KeyboardInterrupt()
+
+        frame = image.getCvFrame()
+        ctrl = dai.message.CameraControl()
+
+        # Check for the key press to toggle the neural network detection
+        if key == ControlKey.DRAW_DETECTIONS:
+            self.toggle = not self.toggle
+
+        # Draw detections only if the toggle is True
+        if self.toggle:
+            self.draw_detections(frame, detections.detections)
+
+        # Display the frame
+        cv2.putText(frame, f"NN inferencing: {self.toggle}", (20, 20), cv2.FONT_HERSHEY_TRIPLEX, 0.7, self.draw_color)
+        cv2.imshow('Frame', frame)
+        return ctrl
+
+    @staticmethod
+    def normalize_frame(frame, bbox) -> np.ndarray:
+        normalized_values = np.full(len(bbox), frame.shape[0])
+        normalized_values[::2] = frame.shape[1]
+        return (np.clip(np.array(bbox), 0, 1) * normalized_values).astype(int)
+
+    def draw_detections(self, frame, detections):
+        for detection in detections:
+            bbox = self.normalize_frame(frame=frame,bbox=(detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+            cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 20),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, self.draw_color)
+            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), self.draw_color, 2)
+
+pipeline = dai.Pipeline()
+with pipeline:
+    feedback = dai.node.Feedback()
+    camera_control = dai.node.ColorCamera(inputControl=feedback, default_output="preview")
+    nn_detections = dai.node.MobileNetDetectionNetwork(
+        input=camera_control.preview,
+        blob_path=blobconverter.from_zoo(name="mobilenet-ssd", shaves=6)
+    )
+    flow = PipelineFlowChangerNode(image=camera_control.preview, detections=nn_detections)
+    feedback.attach(camera_control)
+    feedback.attach(flow)
+dai.run(pipeline)
