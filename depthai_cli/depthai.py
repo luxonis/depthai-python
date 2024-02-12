78e3557f1d34e9f346588e748b2c7085c383e9fd
zrezke <jeretina.filip@gmail.com>
2024-02-12 10:55:27 +0100

    --list-devices -> list connected devices instead of available.

--- depthai_cli/depthai.py
+++ depthai_cli/depthai.py
@@ -5,13 +5,13 @@ def cli():
 
     parser = argparse.ArgumentParser(description="DepthAI CLI")
     parser.add_argument("-v", "--version", action="store_true", help="Print version and exit.")
-    parser.add_argument("-l", "--list-devices", action="store_true", help="List available devices.")
+    parser.add_argument("-l", "--list-devices", action="store_true", help="List connected devices.")
 
     args = parser.parse_args()
     if args.version:
         print(dai.__version__)
     elif args.list_devices:
-        print(dai.Device.getAllAvailableDevices())
+        print(dai.Device.getAllConnectedDevices())
     elif len(sys.argv) == 1:
         parser.print_help()
 
