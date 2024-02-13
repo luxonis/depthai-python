97e54dc06a96665a097b6f87d57f74602b9bb0a8
zrezke <jeretina.filip@gmail.com>
2024-02-13 14:11:56 +0100

    Fix

--- examples/NeuralNetwork/thermal_nnet.py
+++ examples/NeuralNetwork/thermal_nnet.py
@@ -8,8 +8,8 @@ device = dai.Device()
 pipeline = dai.Pipeline()
 nnet = pipeline.create(dai.node.YoloDetectionNetwork)
 nnet.setBlobPath(Path("~/Downloads/yolov6n_sim.blob").expanduser())
-nnet.setConfidenceThreshold(0.1)
-nnet.setNumClasses(6)
+nnet.setConfidenceThreshold(0.5)
+nnet.setNumClasses(1)
 nnet.setCoordinateSize(4)
 nnet.setIouThreshold(0.5)
 
@@ -29,6 +29,7 @@ xin_nn.out.link(nnet.input)
 raw_out = pipeline.createXLinkOut()
 raw_out.setStreamName("preview")
 thermal_cam.preview.link(raw_out.input)
+
 
 
 xout_nn = pipeline.createXLinkOut()
@@ -52,7 +53,7 @@ cv2.namedWindow("nnet", cv2.WINDOW_NORMAL)
 cv2.namedWindow("raw", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("nnet", 640, 480)
 cv2.resizeWindow("raw", 640, 480)
-labels = ["walk_person", "ride_person", "squat_person", "people", "person?", "people?"]
+labels = ["person"]
 
 while True:
     in_nn = q_nn.get()
