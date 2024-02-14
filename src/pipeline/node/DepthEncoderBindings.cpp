1e4fe97178ddaf7ce8683eb2dc78163ae48fa408
Matevz Morato <matevz.morato@gmail.com>
2024-02-14 22:53:42 +0000

    Move the hue encoding to FW to allow lazy loading of the disp2Depth

--- src/pipeline/node/DepthEncoderBindings.cpp
+++ src/pipeline/node/DepthEncoderBindings.cpp
@@ -14,6 +14,7 @@ void bind_depthencoder(pybind11::module& m, void* pCallstack){
     // Node and Properties declare upfront
     py::class_<DepthEncoderProperties> depthEncoderProperties(m, "DepthEncoderProperties", DOC(dai, DepthEncoderProperties));
     auto depthEncoder = ADD_NODE(DepthEncoder);
+    py::enum_<HueEncodingType> hueEncodingType(depthEncoderProperties, "HueEncodingType");
 
     ///////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////
@@ -28,63 +29,29 @@ void bind_depthencoder(pybind11::module& m, void* pCallstack){
     ///////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////
 
+    hueEncodingType.value("LINEAR", HueEncodingType::LINEAR, DOC(dai, DepthEncoderProperties, HueEncodingType, LINEAR))
+        .value("LOGARITHMIC", HueEncodingType::LOGARITHMIC, DOC(dai, DepthEncoderProperties, HueEncodingType, LOGARITHMIC));
     // DepthEncoder properties
-    depthEncoderProperties
-        .def_readwrite("outputType", &DepthEncoderProperties::outputType, DOC(dai, DepthEncoderProperties, outputType))
+    depthEncoderProperties.def_readwrite("outputType", &DepthEncoderProperties::outputType, DOC(dai, DepthEncoderProperties, outputType))
         .def_readwrite("numFramesPool", &DepthEncoderProperties::numFramesPool, DOC(dai, DepthEncoderProperties, numFramesPool))
         .def_readwrite("numShaves", &DepthEncoderProperties::numShaves, DOC(dai, DepthEncoderProperties, numShaves))
+        .def_readwrite("useHueEncoding", &DepthEncoderProperties::useHueEncoding, DOC(dai, DepthEncoderProperties, useHueEncoding))
+        .def_readwrite("hueEncodingType", &DepthEncoderProperties::hueEncodingType, DOC(dai, DepthEncoderProperties, hueEncodingType))
+        .def_readwrite("minDepth", &DepthEncoderProperties::minDepth, DOC(dai, DepthEncoderProperties, minDepth))
+        .def_readwrite("maxDepth", &DepthEncoderProperties::maxDepth, DOC(dai, DepthEncoderProperties, maxDepth))
         .def_readwrite("lutR", &DepthEncoderProperties::lutR, DOC(dai, DepthEncoderProperties, lutR))
         .def_readwrite("lutG", &DepthEncoderProperties::lutG, DOC(dai, DepthEncoderProperties, lutG))
-        .def_readwrite("lutB", &DepthEncoderProperties::lutB, DOC(dai, DepthEncoderProperties, lutB))
-        ;
+        .def_readwrite("lutB", &DepthEncoderProperties::lutB, DOC(dai, DepthEncoderProperties, lutB));
 
     // DepthEncoder Node
-    depthEncoder
-        .def_readonly("input", &DepthEncoder::input,
-                      DOC(dai, node, DepthEncoder, input))
-        .def_readonly("output", &DepthEncoder::output,
-                      DOC(dai, node, DepthEncoder, output))
-        .def_readonly("passthroughInput", &DepthEncoder::passthroughInput,
-                      DOC(dai, node, DepthEncoder, passthroughInput))
-        .def("setLut", &DepthEncoder::setLut,
-             DOC(dai, node, DepthEncoder, setLut))
-        // .def("setHueLut", &DepthEncoder::setHueLut, py::arg("minIn"),
-        // py::arg("maxIn"), py::arg("scaleFactor") = 1,
-        // py::arg("bufferPercentage") = 0.0f, DOC(dai, node, DepthEncoder,
-        // setHueLut))
-        .def("setOutputType", &DepthEncoder::setOutputType,
-             DOC(dai, node, DepthEncoder, setOutputType))
-        .def("setNumFramesPool", &DepthEncoder::setNumFramesPool,
-             DOC(dai, node, DepthEncoder, setNumFramesPool))
-        .def("setNumShaves", &DepthEncoder::setNumShaves,
-             DOC(dai, node, DepthEncoder, setNumShaves),
-             DOC(dai, node, DepthEncoder, setNumShaves))
-        .def("setHueLutGeneric", &DepthEncoder::setHueLutGeneric,
-             py::arg("minDepthIn"), py::arg("maxDepthIn"),
-             py::arg("bufferAmount"), py::arg("getMinDisparity"),
-             py::arg("getMaxDisparity"), py::arg("getHueValueFromDisparity"),
-             DOC(dai, node, DepthEncoder, setHueLutGeneric))
-        // Binding for setHueLutDisparity
-        .def("setHueLutDisparity", &DepthEncoder::setHueLutDisparity,
-             py::arg("minInDepth"), py::arg("maxInDepth"), py::arg("scale"),
-             py::arg("bufferAmount"),
-             DOC(dai, node, DepthEncoder, setHueLutDisparity))
-        // Binding for setHueLutDepth
-        .def("setHueLutDepth", &DepthEncoder::setHueLutDepth,
-             py::arg("minInDepth"), py::arg("maxInDepth"), py::arg("scale"),
-             py::arg("bufferAmount"),
-             DOC(dai, node, DepthEncoder, setHueLutDepth))
-        .def(
-            "setHueLutDepthNormalized",
-            [](DepthEncoder &self, float minInDepth, float maxInDepth,
-               float scale, float bufferAmount) {
-              auto result = self.setHueLutDepthNormalized(
-                  minInDepth, maxInDepth, scale, bufferAmount);
-              return py::make_tuple(std::get<0>(result), std::get<1>(result));
-            },
-            py::arg("minInDepth"), py::arg("maxInDepth"), py::arg("scale"),
-            py::arg("bufferAmount"),
-            DOC(dai, node, DepthEncoder, setHueLutDepthNormalized));
+    depthEncoder.def_readonly("input", &DepthEncoder::input, DOC(dai, node, DepthEncoder, input))
+        .def_readonly("output", &DepthEncoder::output, DOC(dai, node, DepthEncoder, output))
+        .def_readonly("passthroughInput", &DepthEncoder::passthroughInput, DOC(dai, node, DepthEncoder, passthroughInput))
+        .def("setLut", &DepthEncoder::setLut, DOC(dai, node, DepthEncoder, setLut))
+        .def("setOutputType", &DepthEncoder::setOutputType, DOC(dai, node, DepthEncoder, setOutputType))
+        .def("setNumFramesPool", &DepthEncoder::setNumFramesPool, DOC(dai, node, DepthEncoder, setNumFramesPool))
+        .def("setNumShaves", &DepthEncoder::setNumShaves, DOC(dai, node, DepthEncoder, setNumShaves), DOC(dai, node, DepthEncoder, setNumShaves))
+        .def("setHueEncoding", &DepthEncoder::setHueEncoding, DOC(dai, node, DepthEncoder, setHueEncoding));
     daiNodeModule.attr("DepthEncoder").attr("Properties") = depthEncoderProperties;
 
 }
