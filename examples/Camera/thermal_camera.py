435d09d5b8194c10758a68ce31a8d5621ccc52ff
zrezke <jeretina.filip@gmail.com>
2024-02-20 14:36:08 +0100

    Convert thermal_camera example from snake to camel case.

--- examples/Camera/thermal_camera.py
+++ examples/Camera/thermal_camera.py
@@ -2,45 +2,45 @@ import depthai as dai
 import cv2
 import numpy as np
 
-mouse_x, mouse_y = 0, 0
+mouseX, mouseY = 0, 0
 
 
-def on_mouse(event, x, y, *args):
-    global mouse_x, mouse_y
-    mouse_x = x
-    mouse_y = y
+def onMouse(event, x, y, *args):
+    global mouseX, mouseY
+    mouseX = x
+    mouseY = y
 
 
 device = dai.Device()
 pipeline = dai.Pipeline()
 
 # Thermal camera
-thermal_cam = pipeline.create(dai.node.Camera)
+thermalCam = pipeline.create(dai.node.Camera)
 width, height = -1, -1
-thermal_found = False
+thermalFound = False
 for features in device.getConnectedCameraFeatures():
     if dai.CameraSensorType.THERMAL in features.supportedTypes:
-        thermal_found = True
-        thermal_cam.setBoardSocket(features.socket)
+        thermalFound = True
+        thermalCam.setBoardSocket(features.socket)
         width, height = features.width, features.height
         break
-if not thermal_found:
+if not thermalFound:
     raise RuntimeError("No thermal camera found!")
-thermal_cam.setPreviewSize(width, height)
+thermalCam.setPreviewSize(width, height)
 
 # Output raw: FP16 temperature data (degrees Celsius)
-xout_raw = pipeline.create(dai.node.XLinkOut)
-xout_raw.setStreamName("thermal_raw")
-thermal_cam.raw.link(xout_raw.input)
+xoutRaw = pipeline.create(dai.node.XLinkOut)
+xoutRaw.setStreamName("thermal_raw")
+thermalCam.raw.link(xoutRaw.input)
 
 # Output preview,video, isp: RGB or NV12 or YUV420 thermal image.
-xout_image = pipeline.create(dai.node.XLinkOut)
-xout_image.setStreamName("image")
-thermal_cam.preview.link(xout_image.input)
+xoutImage = pipeline.create(dai.node.XLinkOut)
+xoutImage.setStreamName("image")
+thermalCam.preview.link(xoutImage.input)
 device.startPipeline(pipeline)
 
-q_raw = device.getOutputQueue("thermal_raw", 2, False)
-q_image = device.getOutputQueue("image", 2, False)
+qRaw = device.getOutputQueue("thermal_raw", 2, False)
+qImage = device.getOutputQueue("image", 2, False)
 
 
 RAW_WINDOW_NAME = "temperature"
@@ -52,56 +52,56 @@ cv2.moveWindow(RAW_WINDOW_NAME, 0, 0)
 cv2.resizeWindow(RAW_WINDOW_NAME, width * 4, height * 4)
 cv2.moveWindow(IMAGE_WINDOW_NAME, width * 4, 0)
 cv2.resizeWindow(IMAGE_WINDOW_NAME, width * 4, height * 4)
-cv2.setMouseCallback(RAW_WINDOW_NAME, on_mouse)
-cv2.setMouseCallback(IMAGE_WINDOW_NAME, on_mouse)
+cv2.setMouseCallback(RAW_WINDOW_NAME, onMouse)
+cv2.setMouseCallback(IMAGE_WINDOW_NAME, onMouse)
 
 while True:
-    in_raw = q_raw.get()
-    in_img = q_image.get()
+    inRaw = qRaw.get()
+    inImg = qImage.get()
 
     # Retrieve one point of fp16 data
-    frame = in_raw.getCvFrame().astype(np.float32)
-    colormapped_frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    colormapped_frame = cv2.applyColorMap(colormapped_frame, cv2.COLORMAP_MAGMA)
+    frame = inRaw.getCvFrame().astype(np.float32)
+    colormappedFrame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    colormappedFrame = cv2.applyColorMap(colormappedFrame, cv2.COLORMAP_MAGMA)
     if (
-        mouse_x < 0
-        or mouse_y < 0
-        or mouse_x >= frame.shape[1]
-        or mouse_y >= frame.shape[0]
+        mouseX < 0
+        or mouseY < 0
+        or mouseX >= frame.shape[1]
+        or mouseY >= frame.shape[0]
     ):
-        mouse_x = max(0, min(mouse_x, frame.shape[1] - 1))
-        mouse_y = max(0, min(mouse_y, frame.shape[0] - 1))
-    text_color = (255, 255, 255)
+        mouseX = max(0, min(mouseX, frame.shape[1] - 1))
+        mouseY = max(0, min(mouseY, frame.shape[0] - 1))
+    textColor = (255, 255, 255)
     # Draw crosshair
     cv2.line(
-        colormapped_frame,
-        (mouse_x - 10, mouse_y),
-        (mouse_x + 10, mouse_y),
-        text_color,
+        colormappedFrame,
+        (mouseX - 10, mouseY),
+        (mouseX + 10, mouseY),
+        textColor,
         1,
     )
     cv2.line(
-        colormapped_frame,
-        (mouse_x, mouse_y - 10),
-        (mouse_x, mouse_y + 10),
-        text_color,
+        colormappedFrame,
+        (mouseX, mouseY - 10),
+        (mouseX, mouseY + 10),
+        textColor,
         1,
     )
     # Draw deg C
-    text = f"{frame[mouse_y, mouse_x]:.2f} deg C"
-    put_text_left = mouse_x > colormapped_frame.shape[1] / 2
+    text = f"{frame[mouseY, mouseX]:.2f} deg C"
+    putTextLeft = mouseX > colormappedFrame.shape[1] / 2
     cv2.putText(
-        colormapped_frame,
+        colormappedFrame,
         text,
-        (mouse_x - 100 if put_text_left else mouse_x + 10, mouse_y - 10),
+        (mouseX - 100 if putTextLeft else mouseX + 10, mouseY - 10),
         cv2.FONT_HERSHEY_SIMPLEX,
         0.5,
-        text_color,
+        textColor,
         1,
     )
-    cv2.imshow(RAW_WINDOW_NAME, colormapped_frame)
+    cv2.imshow(RAW_WINDOW_NAME, colormappedFrame)
 
-    cv2.imshow(IMAGE_WINDOW_NAME, in_img.getCvFrame())
+    cv2.imshow(IMAGE_WINDOW_NAME, inImg.getCvFrame())
 
     if cv2.waitKey(1) == ord("q"):
         break
