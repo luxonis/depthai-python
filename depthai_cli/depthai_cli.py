91c641ee18337d3b3225c2c28ce1520ba610068a
zrezke <jeretina.filip@gmail.com>
2024-02-26 12:30:59 +0100

    Use subparsers for subcommands (like cam_test) to print better help.

--- depthai_cli/depthai_cli.py
+++ depthai_cli/depthai_cli.py
@@ -13,32 +13,37 @@ else:
 CAM_TEST_PATH = str(CAM_TEST_PATH)
 
 
-def cli():
+def cli() -> int:
     import argparse
     import sys
     import depthai as dai
-    parser = argparse.ArgumentParser(description="DepthAI CLI", add_help=False)
+    parser = argparse.ArgumentParser(description="DepthAI CLI", add_help=True)
     parser.add_argument("-v", "--version", action="store_true", help="Print version and exit.")
     parser.add_argument("-l", "--list-devices", action="store_true", help="List connected devices.")
-    parser.add_argument("commands", nargs=argparse.REMAINDER, help="Commands and options for cam_test")
+    subparsers = parser.add_subparsers(dest="command", help="Sub-commands")
+    # Define the parser for the "cam_test" command
+    cam_test_parser = subparsers.add_parser("cam_test", help="Commands and options for cam_test", add_help=False)
+    cam_test_parser.add_argument("args", nargs=argparse.REMAINDER, help="Arguments to pass to cam_test")
 
-    args = parser.parse_args()
-    if args.version:
-        print(dai.__version__)
-        return
-    if args.list_devices:
-        print(dai.Device.getAllConnectedDevices())
-        return
-
-    # Handle cam_test command
-    if args.commands and args.commands[0] == "cam_test":
+    # subparser REMINDER args would get parsed too if we used parse_args, so we have to handle unknown args manually
+    args, unknown_args = parser.parse_known_args()
+    if args.command == "cam_test":
         cam_test_path = CAM_TEST_PATH
-        # Forward the arguments to cam_test.py, excluding the "cam_test" command itself
-        subprocess.run([sys.executable, cam_test_path] + args.commands[1:])
+        return subprocess.run([sys.executable, cam_test_path] + cam_test_parser.parse_args().args[1:]).returncode
+    # Parse other subcommands here
+    elif unknown_args:
+        parser.error(f"Unrecognized arguments: {unknown_args}") # handles exit internally
+    elif args.version:
+        print(dai.__version__)
+        return 0
+    elif args.list_devices:
+        print(dai.Device.getAllConnectedDevices())
+        return 0
     else:
         # No recognized commands, print help
         parser.print_help()
-        print("To run cam_test, use: depthai cam_test [options for cam_test]")
+        return 1
+
 
 if __name__ == "__main__":
-    cli()
+    exit(cli())
