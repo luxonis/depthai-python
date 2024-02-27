f2a7cc4f6586409f3cc46cd750a17d4739be4e94
jakaskerl <53253318+jakaskerl@users.noreply.github.com>
2024-02-27 18:29:16 +0100

    Update thermal_nnet.py
    - removed unused nodes
    - made all variables camelCase for consistency

--- examples/NeuralNetwork/thermal_nnet.py
+++ examples/NeuralNetwork/thermal_nnet.py
@@ -3,71 +3,58 @@ import cv2
 from pathlib import Path
 import numpy as np
 
+labels = ["person"]
 
 device = dai.Device()
+
 pipeline = dai.Pipeline()
 nnet = pipeline.create(dai.node.YoloDetectionNetwork)
-nnet.setBlobPath(Path("~/Downloads/yolov6n_sim.blob").expanduser())
+nnet.setBlobPath(Path("./yolov6n_thermal_people_256x192.blob").expanduser())
 nnet.setConfidenceThreshold(0.5)
 nnet.setNumClasses(1)
 nnet.setCoordinateSize(4)
-nnet.setIouThreshold(0.5)
+nnet.setIouThreshold(0.4)
 
-thermal_cam = pipeline.create(dai.node.Camera)
-thermal_cam.setBoardSocket(dai.CameraBoardSocket.CAM_E)
-thermal_cam.setPreviewSize(256, 192)
+thermalCam = pipeline.create(dai.node.Camera)
+thermalCam.setBoardSocket(dai.CameraBoardSocket.CAM_E)
+thermalCam.setPreviewSize(256, 192)
 
-thermal_cam.raw.link(nnet.input)
-xout_raw = pipeline.createXLinkOut()
-xout_raw.setStreamName("raw")
+thermalCam.raw.link(nnet.input)
 
-xin_nn = pipeline.createXLinkIn()
-xin_nn.setStreamName("in_nn")
-xin_nn.out.link(nnet.input)
+rawOut = pipeline.createXLinkOut()
+rawOut.setStreamName("preview")
+thermalCam.preview.link(rawOut.input)
 
+xoutNn = pipeline.createXLinkOut()
+xoutNn.setStreamName("nn")
+nnet.out.link(xoutNn.input)
 
-raw_out = pipeline.createXLinkOut()
-raw_out.setStreamName("preview")
-thermal_cam.preview.link(raw_out.input)
-
-
-
-xout_nn = pipeline.createXLinkOut()
-xout_nn.setStreamName("nn")
-nnet.out.link(xout_nn.input)
-
-xout_pass = pipeline.createXLinkOut()
-xout_pass.setStreamName("pass")
-nnet.passthrough.link(xout_pass.input)
+xoutPass = pipeline.createXLinkOut()
+xoutPass.setStreamName("pass")
+nnet.passthrough.link(xoutPass.input)
 
 device.startPipeline(pipeline)
 
-q_nn = device.getOutputQueue(name="nn", maxSize=2, blocking=False)
-q_pass = device.getOutputQueue(name="pass", maxSize=2, blocking=False)
-q_preview = device.getOutputQueue(name="preview", maxSize=2, blocking=False)
-q_in_nn = device.getInputQueue("in_nn")
-q_raw = device.getOutputQueue(name="raw", maxSize=2, blocking=False)
-
+qNn = device.getOutputQueue(name="nn", maxSize=2, blocking=False)
+qPass = device.getOutputQueue(name="pass", maxSize=2, blocking=False)
+qPreview = device.getOutputQueue(name="preview", maxSize=2, blocking=False)
 
 cv2.namedWindow("nnet", cv2.WINDOW_NORMAL)
 cv2.namedWindow("raw", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("nnet", 640, 480)
 cv2.resizeWindow("raw", 640, 480)
-labels = ["person"]
 
 while True:
-    in_nn = q_nn.get()
-    in_pass = q_pass.tryGet()
-    in_preview = q_preview.get()
-    if in_nn and in_pass:
-        frame = in_pass.getCvFrame().astype(np.float32)
+    inNn = qNn.get()
+    inPass = qPass.tryGet()
+    inPreview = qPreview.get()
+    if inNn and inPass:
+        frame = inPass.getCvFrame().astype(np.float32)
         min_, max_ = frame.min(), frame.max()
-        colormapped_frame = cv2.normalize(
-            frame, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U
-        )
-        colormapped_frame = cv2.applyColorMap(colormapped_frame, cv2.COLORMAP_MAGMA)
+        colormappedFrame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        colormappedFrame = cv2.applyColorMap(colormappedFrame, cv2.COLORMAP_MAGMA)
 
-        detections = in_nn.detections
+        detections = inNn.detections
         for detection in detections:
             xmin = max(0.0, detection.xmin)
             ymin = max(0.0, detection.ymin)
@@ -75,19 +62,11 @@ while True:
             ymax = min(1.0, detection.ymax)
             pt1 = int(xmin * 256), int(ymin * 192)
             pt2 = int(xmax * 256), int(ymax * 192)
-            cv2.rectangle(colormapped_frame, pt1, pt2, (0, 255, 0))
-            cv2.putText(
-                colormapped_frame,
-                labels[detection.label],
-                pt1,
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (0, 255, 0),
-                2,
-                cv2.LINE_AA,
-            )
-        cv2.imshow("nnet", colormapped_frame)
-    if in_preview:
-        cv2.imshow("raw", in_preview.getCvFrame())
+            cv2.rectangle(colormappedFrame, pt1, pt2, (0, 255, 0))
+            cv2.putText(colormappedFrame, labels[detection.label], pt1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.imshow("nnet", colormappedFrame)
+    if inPreview:
+        cv2.imshow("raw", inPreview.getCvFrame())
+
     if cv2.waitKey(1) == ord("q"):
         break
